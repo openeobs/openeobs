5a040d4f2c9663c78012129c8847c20aec8f6aa7
Neova Health <jenkins@neovahealth.co.uk>
2014-03-27 10:20:10 +0000

    no db

--- t4clinical_demo/create_demo_database.py
+++ t4clinical_demo/create_demo_database.py
@@ -1,17 +1,17 @@
 import erppeek
 
 
-new_db = "demo_test40"
+new_db = "demo_test41"
 new_demo = False
 rollback = False
 
 user = "admin"
 pwd = "admin"
-current_db = "test11"
+#current_db = "test11"
 
  
 
-client = erppeek.Client("http://localhost:8069", current_db, user, pwd, verbose=True)
+client = erppeek.Client("http://localhost:8069", verbose=True)
 client.create_database(pwd, new_db, new_demo, 'en_GB', pwd)
 client.install('t4clinical_demo')
 client.execute('demo', 'scenario1', rollback)
