acdc0c04ad3bbd48f8f2dfacc592c3838c805dca
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-04-16 18:07:16 +0100

    [IMP] t4clinical_default_config initial commit.

---
+++ t4clinical_default_config/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'T4 Clinical Default Configuration',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Tactix4',
+    'website': 'http://www.tactix4.com/',
+    'depends': ['t4clinical_base','t4clinical_activity_types'],
+    'data': ['views.xml',
+             'menuitem.xml'],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
