fd83bb1860742e4fb8527b55a5421eb0543f1cd2
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-05-01 18:09:17 +0100

    [IMP] API activity calls

--- t4clinical_api/api.py
+++ t4clinical_api/api.py
@@ -1,4 +1,5 @@
-from openerp.osv import orm
+from openerp.osv import orm, osv
+from openerp.tools.translate import _
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -7,6 +8,10 @@ _logger = logging.getLogger(__name__)
 class t4_clinical_api(orm.AbstractModel):
     _name = 't4.clinical.api'
     _inherit = 't4.clinical.api'
+
+    # # # # # # # #
+    #  ACTIVITIES #
+    # # # # # # # #
 
     def getActivities(self, cr, uid, ids, context=None):
         domain = [('id', 'in', ids)] if ids else []
@@ -19,6 +24,50 @@ class t4_clinical_api(orm.AbstractModel):
         activity_pool = self.pool['t4.clinical.activity']
         domain = [('id', '=', activity_id)]
         activity_ids = activity_pool.search(cr, uid, domain, context=context)
-        return activity_pool.cancel(cr, uid, activity_id, context=context) if activity_ids else False
+        if not activity_ids:
+            raise osv.except_osv(_('Error!'), 'Activity ID not found: %s' % activity_id)
+        return activity_pool.cancel(cr, uid, activity_id, context=context)
 
-    # def submit(self, cr, uid, activity_id, data, context=None):
+    def submit(self, cr, uid, activity_id, data, context=None):
+        activity_pool = self.pool['t4.clinical.activity']
+        domain = [('id', '=', activity_id)]
+        activity_ids = activity_pool.search(cr, uid, domain, context=context)
+        if not activity_ids:
+            raise osv.except_osv(_('Error!'), 'Activity ID not found: %s' % activity_id)
+        return activity_pool.submit(cr, uid, activity_id, data, context=context)
+
+    def unassign(self, cr, uid, activity_id, context=None):
+        activity_pool = self.pool['t4.clinical.activity']
+        domain = [('id', '=', activity_id)]
+        activity_ids = activity_pool.search(cr, uid, domain, context=context)
+        if not activity_ids:
+            raise osv.except_osv(_('Error!'), 'Activity ID not found: %s' % activity_id)
+        return activity_pool.unassign(cr, uid, activity_id, context=context)
+
+    def assign(self, cr, uid, activity_id, data, context=None):
+        activity_pool = self.pool['t4.clinical.activity']
+        user_pool = self.pool['res.users']
+        user_id = uid
+        domain = [('id', '=', activity_id)]
+        activity_ids = activity_pool.search(cr, uid, domain, context=context)
+        if not activity_ids:
+            raise osv.except_osv(_('Error!'), 'Activity ID not found: %s' % activity_id)
+        if data.get('user_id'):
+            user_id = data['user_id']
+            domain = [('id', '=', user_id)]
+            user_ids = user_pool.search(cr, uid, domain, context=context)
+            if not user_ids:
+                raise osv.except_osv(_('Error!'), 'User ID not found: %s' % user_id)
+        return activity_pool.assign(cr, uid, activity_id, user_id, context=context)
+
+    def complete(self, cr, uid, activity_id, data, context=None):
+        activity_pool = self.pool['t4.clinical.activity']
+        domain = [('id', '=', activity_id)]
+        activity_ids = activity_pool.search(cr, uid, domain, context=context)
+        if not activity_ids:
+            raise osv.except_osv(_('Error!'), 'Activity ID not found: %s' % activity_id)
+        return activity_pool.complete(cr, uid, activity_id, data, context=context)
+
+    # # # # # # #
+    #  PATIENTS #
+    # # # # # # #
--- t4clinical_api/tests/__init__.py
+++ t4clinical_api/tests/__init__.py
@@ -2,6 +2,5 @@ import test_t4clinical_controller
 
 
 fast_suite = [
-    # CONTROLLER NOT FINISHED YET
     # test_t4clinical_controller
     ]
