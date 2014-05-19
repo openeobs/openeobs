bc8aa872de013dd9b7e33375e3e133a3b22895c3
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-05-19 13:03:28 +0100

    [IMP][TASK 852] merge, transfer & createActivity

--- t4clinical_api/api.py
+++ t4clinical_api/api.py
@@ -2,6 +2,7 @@ from openerp.osv import orm, osv, fields
 from datetime import datetime as dt, timedelta as td
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 from openerp.tools.translate import _
+from openerp import SUPERUSER_ID
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -110,7 +111,7 @@ class t4_clinical_api(orm.AbstractModel):
             _logger.warn("Patient registered from an update call - data available:%s" % data)
             self.register(cr, uid, patient_id, data, context=context)
         data.update({'other_identifier': patient_id})
-        update_activity = self._create_activity('t4.clinical.adt.patient.update', {}, {})
+        update_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.update', {}, {}, context=context)
         activity_pool.submit(cr, uid, update_activity, data, context=context)
         activity_pool.complete(cr, uid, update_activity, context=context)
         _logger.info("Patient updated\n data: %s" % data)
@@ -118,7 +119,7 @@ class t4_clinical_api(orm.AbstractModel):
 
     def register(self, cr, uid, patient_id, data, context=None):
         activity_pool = self.pool['t4.activity']
-        register_activity = self._create_activity('t4.clinical.adt.patient.register', {}, {})
+        register_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.register', {}, {}, context=context)
         data.update({'other_identifier': patient_id})
         activity_pool.submit(cr, uid, register_activity, data, context=context)
         activity_pool.complete(cr, uid, register_activity, context=context)
@@ -131,7 +132,7 @@ class t4_clinical_api(orm.AbstractModel):
             _logger.warn("Patient registered from an admit call")
             self.register(cr, uid, patient_id, {}, context=context)
         data.update({'other_identifier': patient_id})
-        admit_activity = self._create_activity('t4.clinical.adt.patient.admit', {}, {})
+        admit_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.admit', {}, {}, context=context)
         activity_pool.submit(cr, uid, admit_activity, data, context=context)
         activity_pool.complete(cr, uid, admit_activity, context=context)
         _logger.info("Patient admitted\n data: %s" % data)
@@ -141,9 +142,31 @@ class t4_clinical_api(orm.AbstractModel):
         if not self._check_hospital_number(cr, uid, patient_id, context=context):
             raise osv.except_osv(_('Error!'), 'Patient ID not found: %s' % patient_id)
         activity_pool = self.pool['t4.activity']
-        discharge_activity = self._create_activity('t4.clinical.adt.patient.discharge', {}, {'other_identifier': patient_id})
+        discharge_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.discharge', {}, {'other_identifier': patient_id}, context=context)
         activity_pool.complete(cr, uid, discharge_activity, context=context)
         _logger.info("Patient discharged: %s" % patient_id)
+        return True
+
+    def merge(self, cr, uid, patient_id, data, context=None):
+        if not self._check_hospital_number(cr, uid, patient_id, context=context):
+            raise osv.except_osv(_('Error!'), 'Patient ID not found: %s' % patient_id)
+        activity_pool = self.pool['t4.activity']
+        data.update({'into_identifier': patient_id})
+        merge_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.merge', {}, {}, context=context)
+        activity_pool.submit(cr, uid, merge_activity, data, context=context)
+        activity_pool.complete(cr, uid, merge_activity, context=context)
+        _logger.info("Patient merged\n data: %s" % data)
+        return True
+
+    def transfer(self, cr, uid, patient_id, data, context=None):
+        if not self._check_hospital_number(cr, uid, patient_id, context=context):
+            raise osv.except_osv(_('Error!'), 'Patient ID not found: %s' % patient_id)
+        activity_pool = self.pool['t4.activity']
+        data.update({'other_identifier': patient_id})
+        transfer_activity = self._create_activity(cr, uid, 't4.clinical.adt.patient.transfer', {}, {}, context=context)
+        activity_pool.submit(cr, uid, transfer_activity, data, context=context)
+        activity_pool.complete(cr, uid, transfer_activity, context=context)
+        _logger.info("Patient transferred\n data: %s" % data)
         return True
 
     def getActivitiesForPatient(self, cr, uid, patient_id, activity_type, start_date=dt.now()+td(days=-30),
@@ -151,14 +174,32 @@ class t4_clinical_api(orm.AbstractModel):
         """
         Returns a list of activities in dictionary format (containing every field from the table)
         :param patient_id: Postgres ID of the patient to get the activities from.
-        :param activity_type: Type of activity we want. Must be an observation.
+        :param activity_type: Type of activity we want.
         :param start_date: start date to filter. A month from now by default.
         :param end_date: end date to filter. Now by default.
         """
-        model_pool = self.pool['t4.clinical.patient.observation.'+activity_type]
-        ids = model_pool.search(cr, uid, [
+        model_pool = self.pool['t4.clinical.patient.observation.'+activity_type] if activity_type else self.pool['t4.activity']
+        domain = [
             ('patient_id', '=', patient_id),
             ('state', '=', 'completed'),
             ('date_terminated', '>=', start_date.strftime(DTF)),
-            ('date_terminated', '<=', end_date.strftime(DTF))], context=context)
+            ('date_terminated', '<=', end_date.strftime(DTF))] if activity_type \
+            else [('patient_id', '=', patient_id), ('state', 'not in', ['completed', 'cancelled'])]
+        ids = model_pool.search(cr, uid, domain, context=context)
         return model_pool.read(cr, uid, ids, [], context=context)
+
+    def createActivity(self, cr, uid, patient_id, activity_type, context=None):
+        if not activity_type:
+            raise osv.except_osv(_('Error!'), 'Activity type not valid')
+        model_name = 't4.clinical.patient.observation.'+activity_type
+        user_pool = self.pool['res.users']
+        access_pool = self.pool['ir.model.access']
+        user = user_pool.browse(cr, SUPERUSER_ID, uid, context=context)
+        rules_ids = access_pool.search(cr, SUPERUSER_ID, [('model_id', '=', model_name), ('group_id', 'in', user.groups_id)], context=context)
+        if not rules_ids:
+            raise osv.except_osv(_('Error!'), 'Access denied, there are no access rules for these activity type - user groups')
+        rules_values = access_pool.read(cr, SUPERUSER_ID, rules_ids, ['perm_responsibility'], context=context)
+        if not any([r['perm_responsibility'] for r in rules_values]):
+            raise osv.except_osv(_('Error!'), 'Access denied, the user is not responsible for this activity type')
+        return self._create_activity(cr, SUPERUSER_ID, model_name, {}, {'patient_id': patient_id}, context=context)
+
