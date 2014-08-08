56a0409c4852f74ee98fe64d3bdafe74e45dbc2d
Ami Patel <ami@neovahealth.co.uk>
2014-08-08 16:53:49 +0100

    Documentation added

---
+++ t4clinical_activity_types/doc/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,titl:1,modul:0,indic:0,content:[0,1],tabl:0,t4clinical_activity_typ:0,document:0,page:0},objtypes:{},titles:["Welcome to t4clinical_activity_types&#8217;s documentation!","&lt;no title&gt;"],objnames:{},filenames:["index","code"]})
