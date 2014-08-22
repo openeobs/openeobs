f0e49bbb3aef4ba4dbf60edcfa4b2eae1029e009
Colin Wren <colin@neovahealth.co.uk>
2014-08-22 12:46:51 +0100

    Making login tests use the URL structure for like dynamicness and stuff

--- mobile_frontend/tests/test_login.py
+++ mobile_frontend/tests/test_login.py
@@ -1,12 +1,15 @@
 __author__ = 'colin'
 
 import openerp.tests
+import helpers
 
 class TestLogin(openerp.tests.HttpCase):
 
+    # I would use ''.format on these strings instead but for some reason the work console makes format throw a wobbly
+
     def test_mobile_redirect_not_logged_in(self):
-        self.phantom_js('/mobile', 'if(document.location.href === "http://localhost:8169/mobile/login/"){ console.log("ok") }else{ console.log("error") }', 'document')
+        self.phantom_js(helpers.URL_PREFIX, 'if(document.location.href === "http://localhost:8169%s"){ console.log("ok") }else{ console.log("error") }' %(helpers.URLS['login']), 'document')
     def test_mobile_redirect_logged_in(self):
-        self.phantom_js('/mobile', 'if(document.location.href === "http://localhost:8169/mobile/tasks/"){ console.log("ok") }else{ console.log("error") }', 'document', login='admin')
+        self.phantom_js(helpers.URL_PREFIX, 'if(document.location.href === "http://localhost:8169%s"){ console.log("ok") }else{ console.log("error") }' %(helpers.URLS['task_list']), 'document', login='admin')
     def test_mobile_redirect_invalid_login(self):
-        self.phantom_js('/mobile', 'if(document.location.href === "http://localhost:8169/mobile/login/"){ console.log("ok") }else{ console.log("error") }', 'document', login='meh')
+        self.phantom_js(helpers.URL_PREFIX, 'if(document.location.href === "http://localhost:8169%s"){ console.log("ok") }else{ console.log("error") }' %(helpers.URLS['login']), 'document', login='meh')
