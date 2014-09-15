634b48d01ac232f29546996ce10f173a13f657a7
Colin Wren <colin@neovahealth.co.uk>
2014-09-15 18:28:19 +0100

    [IMP] Including all tests for mobile frontend atthe moment

--- mobile_frontend/tests/__init__.py
+++ mobile_frontend/tests/__init__.py
@@ -1,13 +1,17 @@
-#import test_patient_list
+import test_patient_list
 import test_task_list
-#import test_news_observation_form
-#import test_login
-#import test_ajax_endpoints
+import test_news_observation_form
+import test_login
+import test_ajax_score
+import test_ajax_partial_reasons
+import test_ajax_task_cancellation_reasons
 
 fast_suite = [
-    #test_patient_list,
+    test_patient_list,
     test_task_list,
-    #test_news_observation_form,
-    #test_login,
-#    test_ajax_endpoints
+    test_news_observation_form,
+    test_login,
+    test_ajax_score
+    test_ajax_partial_reasons
+    test_ajax_task_cancellation_reasons
 ]
