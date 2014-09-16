1394ed6f5edb2fb50529a624f9e53d6791605397
Colin Wren <colin@neovahealth.co.uk>
2014-09-16 15:30:48 +0100

    [IMP] Assess Patient test

--- mobile_frontend/tests/__init__.py
+++ mobile_frontend/tests/__init__.py
@@ -1,17 +1,21 @@
-import test_patient_list
-import test_task_list
-import test_news_observation_form
-import test_login
-import test_ajax_score
-import test_ajax_partial_reasons
-import test_ajax_task_cancellation_reasons
+# import test_patient_list
+# import test_task_list
+# import test_news_observation_form
+# import test_login
+# import test_ajax_score
+# import test_ajax_partial_reasons
+# import test_ajax_task_cancellation_reasons
+# import test_observation_frequency_form
+import test_assess_patient_form
 
 fast_suite = [
-    test_patient_list,
-    test_task_list,
-    test_news_observation_form,
-    test_login,
-    test_ajax_score
-    test_ajax_partial_reasons
-    test_ajax_task_cancellation_reasons
+    # test_patient_list,
+    # test_task_list,
+    # test_news_observation_form,
+    # test_login,
+    # test_ajax_score
+    # test_ajax_partial_reasons
+    # test_ajax_task_cancellation_reasons
+    # test_observation_frequency_form
+    test_assess_patient_form
 ]
