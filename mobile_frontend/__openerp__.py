d51356d32856d7d541e9202b95eb842b8de9ff32
Colin Wren <colin@neovahealth.co.uk>
2014-09-23 11:36:03 +0100

    [IMP] Adding database dropdown / hidden input so database no longer hardcoded

--- mobile_frontend/__openerp__.py
+++ mobile_frontend/__openerp__.py
@@ -5,8 +5,7 @@
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Mobile Frontend for Open-eObs',
-    'description': """Mobile Frontend for Open-eObs
-    """,
+    'description': """Mobile Frontend for Open-eObs """,
     'author': 'Tactix4',
     'website': 'http://www.tactix4.com/',
     'depends': ['web', 't4clinical_api', 't4clinical_base', 't4clinical_activity_types'],
