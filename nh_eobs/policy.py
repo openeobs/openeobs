35cce6bb2602382b6b583e2cd5e4fed8a49d03f6
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-17 12:22:10 +0100

    [IMP][TASK 1316] eObs policy context

--- nh_eobs/policy.py
+++ nh_eobs/policy.py
@@ -5,14 +5,14 @@ class nh_clinical_patient_placement(orm.Model):
     _name = 'nh.clinical.patient.placement'
     _inherit = 'nh.clinical.patient.placement'
 
-    _POLICY = {'activities': [{'model': 'nh.clinical.patient.observation.ews', 'type': 'recurring'}]}
+    _POLICY = {'activities': [{'model': 'nh.clinical.patient.observation.ews', 'type': 'recurring', 'context': 'eobs'}]}
 
 
 class nh_clinical_patient_admission(orm.Model):
     _name = 'nh.clinical.patient.admission'
     _inherit = 'nh.clinical.patient.admission'
 
-    _POLICY = {'activities': [{'model': 'nh.clinical.patient.placement', 'type': 'schedule',
+    _POLICY = {'activities': [{'model': 'nh.clinical.patient.placement', 'type': 'schedule', 'context': 'eobs',
                                'create_data': {
                                    'suggested_location_id': 'data_ref.suggested_location_id.id'
                                }}]}
