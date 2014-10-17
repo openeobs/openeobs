8191f81a91296718eb3177171b713bfe4121fb01
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-17 10:01:30 +0100

    [IMP][TASK 1314] Context Based Policy Triggers

--- nh_eobs/policy.py
+++ nh_eobs/policy.py
@@ -5,5 +5,14 @@ class nh_clinical_patient_placement(orm.Model):
     _name = 'nh.clinical.patient.placement'
     _inherit = 'nh.clinical.patient.placement'
 
-
     _POLICY = {'activities': [{'model': 'nh.clinical.patient.observation.ews', 'type': 'recurring'}]}
+
+
+class nh_clinical_patient_admission(orm.Model):
+    _name = 'nh.clinical.patient.admission'
+    _inherit = 'nh.clinical.patient.admission'
+
+    _POLICY = {'activities': [{'model': 'nh.clinical.patient.placement', 'type': 'schedule',
+                               'create_data': {
+                                   'suggested_location_id': 'data_ref.suggested_location_id.id'
+                               }}]}
