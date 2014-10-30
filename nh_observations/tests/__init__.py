e941bfefdbe8322ce09320d7ae91790180984397
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-30 14:34:15 +0000

    [FIX] API testing

--- nh_observations/tests/__init__.py
+++ nh_observations/tests/__init__.py
@@ -1,5 +1,4 @@
 import test_observations
-import test_api
 import test_demo
 import test_new_demo
 import test_activity_access
@@ -8,7 +7,6 @@ import test_activity_access
  
 fast_suite = [
     test_observations,
-    test_api,
     test_demo,
     test_new_demo,
     test_activity_access,
