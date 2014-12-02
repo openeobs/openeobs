92d42190127205dd7a98de578b4a0c9668ea2136
Colin Wren <colin@neovahealth.co.uk>
2014-12-02 14:47:04 +0000

    Adding new graph lib to report

--- nh_eobs/controllers/visit_report.py
+++ nh_eobs/controllers/visit_report.py
@@ -1,4 +1,4 @@
-__author__ = 'colin'
+# -*- coding: utf-8 -*-
 
 import openerp, json
 from openerp import http
@@ -11,70 +11,112 @@ from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as dtf
 endpoint = '/visit_report/'
 
 graph_js = """
-    var records = {json};
-    var svg = graph_lib.svg,focus = graph_lib.focus,context = graph_lib.context;
-    context.height = 85;
-    context.margins.bottom = 28;
-    focus.chartHeight = 80;
-    focus.chartPadding = 20;
-    svg.focusOnly = false;
-    svg.margins.left = 30;
-    context.margins.top = 20;
-    focus.margins.top = 45;
-    printing = true;
-    graph_lib.svg.printing = true;
-    focus.graphs = null;
-    focus.graphs = new Array();
-    focus.tables = null;
-    focus.tables = new Array();
+    var svg = new window.NH.NHGraphLib('#chart');
+    var resp_rate_graph = new window.NH.NHGraph();
+    resp_rate_graph.options.keys = ['respiration_rate'];
+    resp_rate_graph.options.label = 'RR';
+    resp_rate_graph.options.measurement = '/min';
+    resp_rate_graph.axes.y.min = 0;
+    resp_rate_graph.axes.y.max = 60;
+    resp_rate_graph.options.normal.min = 12;
+    resp_rate_graph.options.normal.max = 20;
+    resp_rate_graph.style.dimensions.height = 200;
+    resp_rate_graph.style.data_style = 'linear';
+    resp_rate_graph.style.label_width = 60;
 
-    context.earliestDate = svg.startParse(records[0].date_started);
-    context.now = svg.startParse(records[records.length-1].date_started);
-    var plotO2 = false;
-    context.scoreRange = [{{"class": "green",s: 0,e: 5}},{{"class": "amber",s: 5,e: 7}},{{"class": "red",s: 7,e: 18}}];
-    records.forEach(function(d){{
-    d.date_started = svg.startParse(d.date_started);
-    if (d.flow_rate !== false || d.concentration !== false){{
-    plotO2 = true;
-    d.inspired_oxygen = "";
-    if(d.flow_rate !== false){{
-        d.inspired_oxygen += "Flow: " + d.flow_rate + "l/hr<br>";
-    }}
-    if(d.concentration !== false){{
-        d.inspired_oxygen += "Concentration: " + d.concentration + "%<br>";
-    }}
-    if(d.cpap_peep !== false){{
-    d.inspired_oxygen += "CPAP PEEP: " + d.cpap_peep + "<br>";
-    }}else if(d.niv_backup !== false){{
-    d.inspired_oxygen += "NIV Backup Rate: " + d.niv_backup + "<br>";
-    d.inspired_oxygen += "NIV EPAP: " + d.niv_epap + "<br>";
-    d.inspired_oxygen += "NIV IPAP: " + d.niv_ipap + "<br>";
-    }}
-    }}
-    if (d.indirect_oxymetry_spo2) {{
-    d.indirect_oxymetry_spo2_label = d.indirect_oxymetry_spo2 + "%";
-    }}
-    }});
+    var oxy_graph = new window.NH.NHGraph()
+    oxy_graph.options.keys = ['indirect_oxymetry_spo2'];
+    oxy_graph.options.label = 'Spo2';
+    oxy_graph.options.measurement = '%';
+    oxy_graph.axes.y.min = 70;
+    oxy_graph.axes.y.max = 100;
+    oxy_graph.options.normal.min = 96;
+    oxy_graph.options.normal.max = 100;
+    oxy_graph.style.dimensions.height = 150;
+    oxy_graph.style.axis.x.hide = true;
+    oxy_graph.style.data_style = 'linear';
+    oxy_graph.style.label_width = 60;
 
-    svg.data = records;
-    focus.graphs.push({{key: "respiration_rate",label: "RR",measurement: "/min",max: 60,min: 0,normMax: 20,normMin: 12}});
-    focus.graphs.push({{key: "indirect_oxymetry_spo2",label: "Spo2",measurement: "%",max: 100,min: 70,normMax: 100,normMin: 96}});
-    focus.graphs.push({{key: "body_temperature",label: "Temp",measurement: "C",max: 50,min: 15,normMax: 37.1,normMin: 35}});
-    focus.graphs.push({{key: "pulse_rate",label: "HR",measurement: "/min",max: 200,min: 30,normMax: 100,normMin: 50}});
-    focus.graphs.push({{key: "blood_pressure",label: "BP",measurement: "mmHg",max: 260,min: 30,normMax: 150,normMin: 50}});
-    focus.tables.push({{
-    key: "avpu_text",
-    label: "AVPU"
-    }});
-    focus.tables.push({{
-    key: "indirect_oxymetry_spo2_label",
-    label: "Oxygen saturation"
-    }});
-    graph_lib.initGraph(18);
-    if (plotO2==true){{
-    focus.tables.push({{key:"inspired_oxygen", label:"Inspired oxygen"}});
-    }}
-    //graph_lib.initTable();
+    var temp_graph = new window.NH.NHGraph();
+    temp_graph.options.keys = ['body_temperature'];
+    temp_graph.options.label = 'Temp';
+    temp_graph.options.measurement = '°C';
+    temp_graph.axes.y.min = 15;
+    temp_graph.axes.y.max = 50;
+    temp_graph.options.normal.min = 35;
+    temp_graph.options.normal.max = 37.1;
+    temp_graph.style.dimensions.height = 150;
+    temp_graph.style.axis.x.hide = true;
+    temp_graph.style.data_style = 'linear';
+    temp_graph.style.label_width = 60;
+
+    var pulse_graph = new window.NH.NHGraph();
+    pulse_graph.options.keys = ['pulse_rate'];
+    pulse_graph.options.label = 'HR';
+    pulse_graph.options.measurement = '/min';
+    pulse_graph.axes.y.min = 30;
+    pulse_graph.axes.y.max = 200;
+    pulse_graph.options.normal.min = 50;
+    pulse_graph.options.normal.max = 100;
+    pulse_graph.style.dimensions.height = 150;
+    pulse_graph.style.axis.x.hide = true;
+    pulse_graph.style.data_style = 'linear';
+    pulse_graph.style.label_width = 60;
+
+    var bp_graph = new window.NH.NHGraph();
+    bp_graph.options.keys = ['blood_pressure_systolic', 'blood_pressure_diastolic'];
+    bp_graph.options.label = 'BP';
+    bp_graph.options.measurement = 'mmHg';
+    bp_graph.axes.y.min = 30;
+    bp_graph.axes.y.max = 260;
+    bp_graph.options.normal.min = 150;
+    bp_graph.options.normal.max = 151;
+    bp_graph.style.dimensions.height = 150;
+    bp_graph.style.axis.x.hide = true;
+    bp_graph.style.data_style = 'range';
+    bp_graph.style.label_width = 60;
+
+    var score_graph = new window.NH.NHGraph();
+    score_graph.options.keys = ['score'];
+    score_graph.style.dimensions.height = 150;
+    score_graph.style.data_style = 'stepped';
+    score_graph.axes.y.min = 0;
+    score_graph.axes.y.max = 22;
+    score_graph.drawables.background.data =  [
+        {{"class": "green",s: 1, e: 4}},
+        {{"class": "amber",s: 4,e: 6}},
+        {{"class": "red",s: 6,e: 22}}
+    ];
+    score_graph.style.label_width = 60;
+
+    var focus = new window.NH.NHFocus();
+    var context = new window.NH.NHContext();
+    focus.graphs.push(resp_rate_graph);
+    focus.graphs.push(oxy_graph);
+    focus.graphs.push(temp_graph);
+    focus.graphs.push(pulse_graph);
+    focus.graphs.push(bp_graph);
+    focus.title = 'Individual values';
+    focus.style.padding.right = 0;
+    context.graph = score_graph;
+    context.title = 'NEWS Score';
+    svg.focus = focus;
+    svg.context = context;
+    svg.data.raw = {json};
+    svg.init();
+    svg.draw();
+
+
+   var table = new window.NH.NHGraphLib('#table');
+   var tabular_obs = new window.NH.NHTable();
+   tabular_obs.keys = ['avpu_text', 'oxygen_administration_flag'];
+   tabular_obs.title = 'Tabular EWS values';
+   var table_focus = new window.NH.NHFocus();
+   table_focus.tables.push(tabular_obs);
+   table.focus = table_focus;
+   table.data.raw = {json}
+   table.init();
+   table.draw();
 """
 
 phantom_code = """
@@ -128,7 +170,7 @@ class VisitReportController(openerp.addons.web.controllers.main.Home):
             with open(get_module_path('nh_graphs') + '/static/lib/js/d3.js'.format(type=type, resource=resource), 'r') as resource:
                 return request.make_response(resource.read(), headers={'Content-Type': self.mimeifier(type)})
         elif resource == 'graph_lib.js':
-            with open(get_module_path('nh_graphs') + '/static/src/js/patient_graph.js'.format(type=type, resource=resource), 'r') as resource:
+            with open(get_module_path('nh_graphs') + '/static/src/js/nh_graphlib.js'.format(type=type, resource=resource), 'r') as resource:
                 return request.make_response(resource.read(), headers={'Content-Type': self.mimeifier(type)})
         else:
             with open(get_module_path('nh_eobs') + '/static/src/{type}/{resource}'.format(type=type, resource=resource), 'r') as resource:
