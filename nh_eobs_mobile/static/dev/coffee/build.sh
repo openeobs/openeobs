50410d01fcc6834235802ba31b4191b856ce9be3
Colin Wren <colin@neovahealth.co.uk>
2014-12-15 13:36:13 +0000

    Sorting out weird import of NHLib JS

--- nh_eobs_mobile/static/dev/coffee/build.sh
+++ nh_eobs_mobile/static/dev/coffee/build.sh
@@ -6,4 +6,4 @@ coffee -o tests/src/ -b -c *.coffee
 # build libs for browser foo
 # cat nhlib.coffee nhmobile.coffee nhmobileform.coffee nhmobilepatient.coffee nhmodal.coffee | coffee --compile --stdio > playground/nhlib.js
 
-cat nhlib.coffee nhmobile.coffee nhmobileform.coffee nhmobilepatient.coffee nhmodal.coffee nhmobileformloz.coffee | coffee --compile --stdio > playground/nhlib.js
+cat nhlib.coffee nhmobile.coffee nhmobileform.coffee nhmobilepatient.coffee nhmodal.coffee nhmobileformloz.coffee | coffee --compile --stdio > ../../src/js/nhlib.js
