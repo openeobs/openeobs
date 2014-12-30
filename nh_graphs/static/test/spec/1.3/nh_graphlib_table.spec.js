47d49a8d4038724aa816bacab0643c4437806c36
Colin Wren <colin@neovahealth.co.uk>
2014-12-30 11:58:56 +0000

    Adding GraphLib Table Drawing Tests

--- nh_graphs/static/test/spec/1.3/nh_graphlib_table.spec.js
+++ nh_graphs/static/test/spec/1.3/nh_graphlib_table.spec.js
@@ -41,6 +41,49 @@ describe('NHGraphLib - Table', function(){
         write_date: "2014-12-01 16:16:53",
         write_uid: [1, "Administrator"]
     }];
+
+    var more_data = {
+        __last_update: "2014-12-01 17:16:53",
+        activity_id: [700, "NEWS Observation"],
+        avpu_text: "V",
+        blood_pressure_diastolic: 90,
+        blood_pressure_systolic: 130,
+        body_temperature: 41,
+        clinical_risk: "Medium",
+        concentration: false,
+        cpap_peep: false,
+        create_date: "2014-12-01 14:15:13",
+        create_uid: [1, "Administrator"],
+        date_started: "2014-12-01 17:16:42",
+        date_terminated: "2014-12-01 17:16:53",
+        device_id: false,
+        display_name: "False",
+        flow_rate: false,
+        frequency: 240,
+        id: 83,
+        indirect_oxymetry_spo2: 91,
+        is_partial: false,
+        mews_score: 0,
+        name: false,
+        niv_backup: false,
+        niv_epap: false,
+        niv_ipap: false,
+        none_values: "[]",
+        null_values: "['niv_epap', 'concentration', 'flow_rate', 'cpap_peep', 'niv_ipap', 'niv_backup']",
+        order_by: false,
+        oxygen_administration_flag: false,
+        partial_reason: false,
+        patient_id: [11, "Littel, Alfreda"],
+        pulse_rate: 81,
+        respiration_rate: 18,
+        score: 5,
+        state: "completed",
+        terminate_uid: [19, "Nadine Bullock"],
+        three_in_one: true,
+        write_date: "2014-12-01 17:16:53",
+        write_uid: [1, "Administrator"]
+    };
+
     beforeEach(function () {
         var body_el = document.getElementsByTagName('body')[0];
         test = document.createElement('div');
@@ -65,43 +108,112 @@ describe('NHGraphLib - Table', function(){
         graphlib.focus = focus;
         graphlib.init();
 
+        spyOn(graphlib, 'draw').andCallThrough();
         graphlib.draw();
-//        expect(graphlib.draw).toHaveBeenCalled();
-//        expect(graphlib.draw_table).toHaveBeenCalled();
-//        // see if svg element is there
-//        var svg = test.getElementsByTagName('svg');
-//        expect(svg.length).toBe(1);
-//        svg = svg[0];
-//        svg_height = svg.getAttribute('height');
-//        svg_width = svg.getAttribute('width');
-//        expect(svg_height).toBe('0');
-//        expect(svg_width).toBe(test.clientWidth.toString());
-//        expect(svg.hasChildNodes()).toBe(false);
-//
-//        // see if popup element is there
-//        var popup = document.getElementById('chart_popup');
-//        expect(popup.classList.contains('hidden')).toBe(true)
-//
-//        // see if table is there
-//        var table = document.getElementById('table');
-//        expect(typeof(table)).toBe('object');
-//
-//        // see if cards are there
-//        expect(table_container.hasChildNodes()).toBe(true);
-//        var cards = document.getElementsByClassName('card');
-//        expect(cards.length).toBe(1);
-//        cards = cards[0];
-//        var card_title = cards.getElementsByTagName('h3');
-//        expect(card_title.length).toBe(1);
-//        card_title = card_title[0];
-//        expect(card_title.textContent).toBe('16:16 01/12/2014');
-//        var card_table = cards.getElementsByTagName('table');
-//        expect(card_table.length).toBe(2); // there's a table inside the main table
-//        card_table = card_table[0];
-//        expect(card_table.hasChildNodes()).toBe(true);
-//        expect(card_table.getElementsByTagName('tr').length).toBe(9);
+        expect(graphlib.draw).toHaveBeenCalled();
+
+        // check that title is there
+        var title_el = test.getElementsByTagName('h3');
+        expect(title_el.length).toBe(1);
+        title_el = title_el[0];
+        expect(title_el.textContent).toBe('Tabular values');
+
+        // check that table is there and is properly coded
+        var table_el = test.getElementsByClassName('nhtable');
+        expect(table_el.length).toBe(1);
+        table_el = table_el[0];
+
+        var thead_el = table_el.getElementsByTagName('thead');
+        expect(thead_el.length).toBe(1);
+        thead_el = thead_el[0];
+
+        var tbody_el = table_el.getElementsByTagName('tbody');
+        expect(tbody_el.length).toBe(1);
+        tbody_el = tbody_el[0];
+
+        // check that table has correct headers
+        var thead_tr = thead_el.getElementsByTagName('tr');
+        expect(thead_tr.length).toBe(1);
+        thead_tr = thead_tr[0];
+        expect(thead_tr.childNodes.length).toBe(3);
+        expect(thead_tr.childNodes[0].textContent).toBe('Date');
+        expect(thead_tr.childNodes[1].textContent).toBe('avpu_text');
+        expect(thead_tr.childNodes[2].textContent).toBe('oxygen_administration_flag');
+
+        //  check that is has correct amount of rows and proper values
+        var tbody_tr = tbody_el.getElementsByTagName('tr');
+        expect(tbody_tr.length).toBe(1);
+        tbody_tr = tbody_tr[0];
+        expect(tbody_tr.childNodes.length).toBe(3);
+        expect(tbody_tr.childNodes[0].textContent).toBe('2014-12-01 16:16:53');
+        expect(tbody_tr.childNodes[1].textContent).toBe('A');
+        expect(tbody_tr.childNodes[2].textContent).toBe('false');
+
     });
 
+    it('draws the table with two sets of data', function(){
+        graphlib.data.raw.push(more_data);
+
+
+        var tabular_obs = new NHTable();
+        tabular_obs.keys = ['avpu_text', 'oxygen_administration_flag', 'pulse_rate'];
+        tabular_obs.title = 'Tabular values';
+        var focus = new NHFocus();
+        focus.tables.push(tabular_obs);
+        graphlib.focus = focus;
+        graphlib.init();
+
+        spyOn(graphlib, 'draw').andCallThrough();
+        graphlib.draw();
+        expect(graphlib.draw).toHaveBeenCalled();
+
+        // check that title is there
+        var title_el = test.getElementsByTagName('h3');
+        expect(title_el.length).toBe(1);
+        title_el = title_el[0];
+        expect(title_el.textContent).toBe('Tabular values');
+
+        // check that table is there and is properly coded
+        var table_el = test.getElementsByClassName('nhtable');
+        expect(table_el.length).toBe(1);
+        table_el = table_el[0];
+
+        var thead_el = table_el.getElementsByTagName('thead');
+        expect(thead_el.length).toBe(1);
+        thead_el = thead_el[0];
+
+        var tbody_el = table_el.getElementsByTagName('tbody');
+        expect(tbody_el.length).toBe(1);
+        tbody_el = tbody_el[0];
+
+        // check that table has correct headers
+        var thead_tr = thead_el.getElementsByTagName('tr');
+        expect(thead_tr.length).toBe(1);
+        thead_tr = thead_tr[0];
+        expect(thead_tr.childNodes.length).toBe(4);
+        expect(thead_tr.childNodes[0].textContent).toBe('Date');
+        expect(thead_tr.childNodes[1].textContent).toBe('avpu_text');
+        expect(thead_tr.childNodes[2].textContent).toBe('oxygen_administration_flag')
+        expect(thead_tr.childNodes[3].textContent).toBe('pulse_rate');
+
+        //  check that is has correct amount of rows and proper values
+        var tbody_tr = tbody_el.getElementsByTagName('tr');
+        expect(tbody_tr.length).toBe(2);
+        var tbody_tr_two = tbody_tr[1];
+        tbody_tr = tbody_tr[0];
+        expect(tbody_tr.childNodes.length).toBe(4);
+        expect(tbody_tr.childNodes[0].textContent).toBe('2014-12-01 16:16:53');
+        expect(tbody_tr.childNodes[1].textContent).toBe('A');
+        expect(tbody_tr.childNodes[2].textContent).toBe('false');
+        expect(tbody_tr.childNodes[3].textContent).toBe('80');
+
+        expect(tbody_tr_two.childNodes.length).toBe(4);
+        expect(tbody_tr_two.childNodes[0].textContent).toBe('2014-12-01 17:16:53');
+        expect(tbody_tr_two.childNodes[1].textContent).toBe('V');
+        expect(tbody_tr_two.childNodes[2].textContent).toBe('false');
+        expect(tbody_tr_two.childNodes[3].textContent).toBe('81');
+
+    });
 
     afterEach(function () {
         if (graphlib != null) {
