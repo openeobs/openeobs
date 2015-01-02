cfb995c1cb6f07ca78c2ed09c1d4df8bfb2393f6
Colin Wren <colin@neovahealth.co.uk>
2015-01-02 16:43:17 +0000

    IE9 useragent before tests so can see in screenshot

--- nh_eobs_mobile/static/dev/coffee/tests/spec/1.3/nhmodal.spec.js
+++ nh_eobs_mobile/static/dev/coffee/tests/spec/1.3/nhmodal.spec.js
@@ -29,7 +29,12 @@ describe('NHModal', function(){
         }
     });
 
-
+    it('tells me what the useragent string is', function(){
+//        expect(navigator.userAgent.indexOf('iPhone') > 0).toBe(true);
+//        expect(navigator.userAgent.indexOf('7_0_') < 0).toBe(true);
+        expect(navigator.userAgent).toBe('meh');
+//        Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_3 like Mac OS X) Apple/WebKit/547.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B508 Safari/9537.53
+    }) ;
 	
 	it('creates a dialog object', function(){
 		var modal = new window.NHModal('id', 'title', 'content', ['<a href="#" data-action="close" data-target="id">Option</a>'], 0, document.getElementById('test'))
