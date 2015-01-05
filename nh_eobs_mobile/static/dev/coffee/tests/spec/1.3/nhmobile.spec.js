a22336d687b1e93e28cfae80dd292dbc7b884b46
Colin Wren <colin@neovahealth.co.uk>
2015-01-05 17:36:14 +0000

    Chrome test was failing as date format wrong

--- nh_eobs_mobile/static/dev/coffee/tests/spec/1.3/nhmobile.spec.js
+++ nh_eobs_mobile/static/dev/coffee/tests/spec/1.3/nhmobile.spec.js
@@ -47,6 +47,8 @@ describe('NHMobile - Object', function() {
         if(navigator.userAgent.indexOf('MSIE') < 0 && navigator.userAgent.indexOf('Trident') < 0) {
             expect(date_for_string.constructor.name).toBe('Date');
         }
+
+
         if((navigator.userAgent.indexOf('Chrome') < 0  && navigator.userAgent.indexOf('Linux') < 0) && (navigator.userAgent.indexOf('iPhone OS') < 0)){
             if((navigator.userAgent.indexOf('MSIE') > 0 || navigator.userAgent.indexOf('Trident') > 0) && navigator.userAgent.indexOf('11') < 0){
                 expect(date_for_string.toString()).toBe('Tue Jan 12 06:00:00 UTC 1988');
@@ -54,7 +56,11 @@ describe('NHMobile - Object', function() {
                 expect(date_for_string.toString()).toBe('Tue Jan 12 1988 06:00:00 GMT+0000 (Coordinated Universal Time)');
             }
         }else{
-            expect(date_for_string.toString()).toBe('Tue Jan 12 1988 06:00:00 GMT+0000 (GMT)');
+            if(navigator.userAgent.indexOf('Chrome') > 0){
+                expect(date_for_string.toString()).toBe('Tue Jan 12 1988 06:00:00 GMT+0000 (Coordinated Universal Time)');
+            }else{
+                expect(date_for_string.toString()).toBe('Tue Jan 12 1988 06:00:00 GMT+0000 (GMT)');
+            }
         }
     });
     
