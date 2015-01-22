ed74a61f164857cc6770f5abc86bca4c5a98b3c0
Colin Wren <colin@neovahealth.co.uk>
2015-01-22 16:08:41 +0000

    Finalising CSS test tool by adding diff for two files so can see what was redundant

--- nh_eobs_kiosk/static/dev/less/build.sh
+++ nh_eobs_kiosk/static/dev/less/build.sh
@@ -5,3 +5,6 @@ lessc src/kiosk_style.less > test/kiosk_style.css
 
 echo "Testing CSS"
 uncss test/styleguide.html > ../../src/css/kiosk_style.css
+
+echo "Redundant Styles"
+diff --suppress-common-lines -i -E -Z -b -B -w test/kiosk_style.css ../../src/css/kiosk_style.css
