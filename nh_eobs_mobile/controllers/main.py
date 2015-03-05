8471100813e802b56022917ea31d54ed04577ca6
Colin Wren <colin@neovahealth.co.uk>
2015-03-05 17:26:15 +0000

    Fixing a bug where HTTPException would try to get cookie assigned to it and thus break

--- nh_eobs_mobile/controllers/main.py
+++ nh_eobs_mobile/controllers/main.py
@@ -669,7 +669,7 @@ class MobileFrontend(openerp.addons.web.controllers.main.Home):
 
         cookie_lifespan = 3600*12 # 12 hours, maybe set in config?
 
-        if response.response:
+        if response.response and not isinstance(response, exceptions.HTTPException):
             response.set_cookie('session_id', httprequest.session.sid, max_age=cookie_lifespan)
         return response
 
