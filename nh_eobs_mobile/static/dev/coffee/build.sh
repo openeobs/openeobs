87f4da936c560937a7a43d4c912a3b6df6af5573
Odoo User <odoo@nhclinical.local>
2015-03-08 00:57:35 +0000

    removing old files from repo

--- nh_eobs_mobile/static/dev/coffee/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# build libs ready for tests
-coffee -o tests/src/ -b -c *.coffee 
-
-# build libs for browser foo
-# cat nhlib.coffee nhmobile.coffee nhmobileform.coffee nhmobilepatient.coffee nhmodal.coffee | coffee --compile --stdio > playground/nhlib.js
-
-cat nhlib.coffee nhmobile.coffee nhmobileform.coffee nhmobilepatient.coffee nhmodal.coffee nhmobileformloz.coffee | coffee --compile --stdio > ../../src/js/nhlib.js
