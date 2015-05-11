87b2692f2e13c9e4ec6f4f33d63bb823ddfbce97
Colin Wren <colin@neovahealth.co.uk>
2015-05-11 17:53:58 +0100

    [WI-577] subclassing view do_action to pass over active_ids from treeview for charts and tables in nh.clinical.wardboard

--- nh_eobs/static/src/js/nh_eobs.js
+++ nh_eobs/static/src/js/nh_eobs.js
@@ -897,4 +897,124 @@ openerp.nh_eobs = function (instance) {
                 return value;
         }
     };
+
+
+    // Override of the do_action implementation in web.View so it actually gets record ids [WI-577]
+    instance.web.View.include({
+        do_execute_action: function (action_data, dataset, record_id, on_closed) {
+            if(dataset.model !== 'nh.clinical.wardboard' || this.fields_view.type !== 'tree'){
+                return this._super(action_data, dataset, record_id, on_closed);
+            }
+
+            var self = this;
+            var result_handler = function () {
+                if (on_closed) { on_closed.apply(null, arguments); }
+                if (self.getParent() && self.getParent().on_action_executed) {
+                    return self.getParent().on_action_executed.apply(null, arguments);
+                }
+            };
+            var context = new instance.web.CompoundContext(dataset.get_context(), action_data.context || {});
+
+            // response handler
+            var handler = function (action) {
+                if (action && action.constructor == Object) {
+                    // filter out context keys that are specific to the current action.
+                    // Wrong default_* and search_default_* values will no give the expected result
+                    // Wrong group_by values will simply fail and forbid rendering of the destination view
+                    var ncontext = new instance.web.CompoundContext(
+                        _.object(_.reject(_.pairs(dataset.get_context().eval()), function(pair) {
+                          return pair[0].match('^(?:(?:default_|search_default_).+|.+_view_ref|group_by|group_by_no_leaf|active_id|active_ids)$') !== null;
+                        }))
+                    );
+                    ncontext.add(action_data.context || {});
+                    ncontext.add({active_model: dataset.model});
+
+                    // Add records from self.records._proxies
+                    var active_ids_to_send = []
+                    if(typeof(self.records._proxies) == 'object'){
+                        for(key in self.records._proxies){
+                            if(self.records._proxies.hasOwnProperty(key)){
+                                for(var i = 0; i < self.records._proxies[key].records.length; i++){
+                                    var rec = self.records._proxies[key].records[i];
+                                    active_ids_to_send.push(rec.attributes.id);
+                                }
+                                //active_ids_to_send = self.records._proxies[key].records;
+                            }
+                        }
+                    }else{
+                        if(record_id){
+                            active_ids_to_send = [record_id]
+                        }
+                    }
+
+
+                    if (record_id) {
+                        ncontext.add({
+                            active_id: record_id,
+                            active_ids: active_ids_to_send,
+                        });
+                    }
+                    ncontext.add(action.context || {});
+                    action.context = ncontext;
+                    return self.do_action(action, {
+                        on_close: result_handler,
+                    });
+                } else {
+                    self.do_action({"type":"ir.actions.act_window_close"});
+                    return result_handler();
+                }
+            };
+
+            if (action_data.special === 'cancel') {
+                return handler({"type":"ir.actions.act_window_close"});
+            } else if (action_data.type=="object") {
+                var args = [[record_id]];
+                if (action_data.args) {
+                    try {
+                        // Warning: quotes and double quotes problem due to json and xml clash
+                        // Maybe we should force escaping in xml or do a better parse of the args array
+                        var additional_args = JSON.parse(action_data.args.replace(/'/g, '"'));
+                        args = args.concat(additional_args);
+                    } catch(e) {
+                        console.error("Could not JSON.parse arguments", action_data.args);
+                    }
+                }
+                args.push(context);
+                return dataset.call_button(action_data.name, args).then(handler).then(function () {
+                    if (instance.webclient) {
+                        instance.webclient.menu.do_reload_needaction();
+                    }
+                });
+            } else if (action_data.type=="action") {
+
+                var active_ids_to_send = []
+                    if(typeof(self.records._proxies) == 'object'){
+                        for(key in self.records._proxies){
+                            if(self.records._proxies.hasOwnProperty(key)){
+                                for(var i = 0; i < self.records._proxies[key].records.length; i++){
+                                    var rec = self.records._proxies[key].records[i];
+                                    active_ids_to_send.push(rec.attributes.id);
+                                }
+                                //active_ids_to_send = self.records._proxies[key].records;
+                            }
+                        }
+                    }else{
+                        if(record_id){
+                            active_ids_to_send = [record_id]
+                        }
+                    }
+
+
+
+
+                return this.rpc('/web/action/load', {
+                    action_id: action_data.name,
+                    context: _.extend(instance.web.pyeval.eval('context', context), {'active_model': dataset.model, 'active_ids': active_ids_to_send, 'active_id': record_id}),
+                    do_not_eval: true
+                }).then(handler);
+            } else  {
+                return dataset.exec_workflow(record_id, action_data.name).then(handler);
+            }
+        },
+    })
 }
