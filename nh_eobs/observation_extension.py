d20710197972dedd6c7747a4318cd8b2c828f378
willearp <will@neovahealth.co.uk>
2015-06-01 17:48:46 +0100

    [IMP] [WI-604] added hooks for observation classed to refresh wardboard query's materialized views. added tests for hooks.

--- nh_eobs/observation_extension.py
+++ nh_eobs/observation_extension.py
@@ -1,12 +1,13 @@
 from openerp.osv import orm
 
-class nh_clinical_patient_observation_ews_refresh(orm.Model):
+
+class nh_clinical_patient_observation_ews(orm.Model):
 
     _name = 'nh.clinical.patient.observation.ews'
     _inherit = 'nh.clinical.patient.observation.ews'
 
     def complete(self, cr, uid, activity_id, context=None):
-        res = super(nh_clinical_patient_observation_ews_refresh, self).complete(cr, uid, activity_id, context=context)
+        res = super(nh_clinical_patient_observation_ews, self).complete(cr, uid, activity_id, context=context)
         sql = """
                 refresh materialized view ews0;
                 refresh materialized view ews1;
@@ -16,4 +17,133 @@ class nh_clinical_patient_observation_ews_refresh(orm.Model):
         return res
 
 
+class nh_clinical_patient_placement(orm.Model):
 
+    _name = 'nh.clinical.patient.placement'
+    _inherit = 'nh.clinical.patient.placement'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_placement, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view placement""")
+        return res
+
+
+class nh_clinical_patient_mrsa(orm.Model):
+
+    _name = 'nh.clinical.patient.mrsa'
+    _inherit = 'nh.clinical.patient.mrsa'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_mrsa, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_diabetes(orm.Model):
+
+    _name = 'nh.clinical.patient.diabetes'
+    _inherit = 'nh.clinical.patient.diabetes'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_diabetes, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_palliative_care(orm.Model):
+
+    _name = 'nh.clinical.patient.palliative_care'
+    _inherit = 'nh.clinical.patient.palliative_care'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_palliative_care, self).complete(cr, uid, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_post_surgery(orm.Model):
+
+    _name = 'nh.clinical.patient.post_surgery'
+    _inherit = 'nh.clinical.patient.post_surgery'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_post_surgery, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_critical_care(orm.Model):
+
+    _name = 'nh.clinical.patient.critical_care'
+    _inherit = 'nh.clinical.patient.critical_care'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_critical_care, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_weight_monitoring(orm.Model):
+
+    _name = 'nh.clinical.patient.weight_monitoring'
+    _inherit = 'nh.clinical.patient.weight_monitoring'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_weight_monitoring, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_urine_output_target(orm.Model):
+
+    _name = 'nh.clinical.patient.uotarget'
+    _inherit = 'nh.clinical.patient.uotarget'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_urine_output_target, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_observation_height(orm.Model):
+
+    _name = 'nh.clinical.patient.observation.height'
+    _inherit = 'nh.clinical.patient.observation.height'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_observation_height, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_pbp_monitoring(orm.Model):
+
+    _name = 'nh.clinical.patient.pbp_monitoring'
+    _inherit = 'nh.clinical.patient.pbp_monitoring'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_pbp_monitoring, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_patient_o2target(orm.Model):
+
+    _name = 'nh.clinical.patient.o2target'
+    _inherit = 'nh.clinical.patient.o2target'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_patient_o2target, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
+
+
+class nh_clinical_o2level(orm.Model):
+
+    _name = 'nh.clinical.o2level'
+    _inherit = 'nh.clinical.o2level'
+
+    def complete(self, cr, uid, activity_id, context=None):
+        res = super(nh_clinical_o2level, self).complete(cr, uid, activity_id, context=context)
+        cr.execute("""refresh materialized view param""")
+        return res
--- nh_eobs/tests/test_observation_extension.py
+++ nh_eobs/tests/test_observation_extension.py
@@ -1,80 +1,77 @@
-from openerp.tests.common import SingleTransactionCase
+from openerp.tests.common import TransactionCase
 from datetime import datetime as dt
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as dtf
+import unittest
 
 
-class TestClinicalPatientObservationEwsRefresh(SingleTransactionCase):
+class TestClinicalPatientObservationEwsRefresh(TransactionCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestClinicalPatientObservationEwsRefresh, cls).setUpClass()
-        cr, uid = cls.cr, cls.uid
+    def setUp(self):
+        super(TestClinicalPatientObservationEwsRefresh, self).setUp()
+        cr, uid = self.cr, self.uid
 
-        cls.users_pool = cls.registry('res.users')
-        cls.groups_pool = cls.registry('res.groups')
-        cls.activity_pool = cls.registry('nh.activity')
-        cls.patient_pool = cls.registry('nh.clinical.patient')
-        cls.location_pool = cls.registry('nh.clinical.location')
-        cls.pos_pool = cls.registry('nh.clinical.pos')
-        cls.spell_pool = cls.registry('nh.clinical.spell')
-        cls.api_pool = cls.registry('nh.clinical.api')
+        self.users_pool = self.registry('res.users')
+        self.activity_pool = self.registry('nh.activity')
+        self.location_pool = self.registry('nh.clinical.location')
+        self.pos_pool = self.registry('nh.clinical.pos')
+        self.spell_pool = self.registry('nh.clinical.spell')
+        self.apidemo = self.registry('nh.clinical.api.demo')
+        self.ews_pool = self.registry('nh.clinical.patient.observation.ews')
+        self.activity_pool = self.registry('nh.activity')
+        self.context_pool = self.registry('nh.clinical.context')
+        self.placement_pool = self.registry('nh.clinical.patient.placement')
+        self.mrsa_pool = self.registry('nh.clinical.patient.mrsa')
+        self.diabetes_pool = self.registry('nh.clinical.patient.diabetes')
+        self.patient_ids = self.apidemo.build_unit_test_env1(cr, uid, bed_count=4, patient_count=4)
+        self.wu_id = self.location_pool.search(cr, uid, [('code', '=', 'U')])[0]
+        self.pos_id = self.location_pool.read(cr, uid, self.wu_id, ['pos_id'])['pos_id'][0]
+        self.patient_id = self.patient_ids[0]
+        self.patient_id2 = self.patient_ids[1]
 
-        cls.apidemo = cls.registry('nh.clinical.api.demo')
-        cls.eobs_api = cls.registry('nh.eobs.api')
-        cls.ews_pool = cls.registry('nh.clinical.patient.observation.ews')
-
-        cls.patient_ids = cls.apidemo.build_unit_test_env1(cr, uid, bed_count=4, patient_count=4)
-
-        cls.wu_id = cls.location_pool.search(cr, uid, [('code', '=', 'U')])[0]
-        cls.wt_id = cls.location_pool.search(cr, uid, [('code', '=', 'T')])[0]
-        cls.pos_id = cls.location_pool.read(cr, uid, cls.wu_id, ['pos_id'])['pos_id'][0]
-        cls.pos_location_id = cls.pos_pool.read(cr, uid, cls.pos_id, ['location_id'])['location_id'][0]
-
-        cls.wmu_id = cls.users_pool.search(cr, uid, [('login', '=', 'WMU')])[0]
-        cls.wmt_id = cls.users_pool.search(cr, uid, [('login', '=', 'WMT')])[0]
-        cls.nu_id = cls.users_pool.search(cr, uid, [('login', '=', 'NU')])[0]
-        cls.nt_id = cls.users_pool.search(cr, uid, [('login', '=', 'NT')])[0]
-        cls.adt_id = cls.users_pool.search(cr, uid, [('groups_id.name', 'in', ['NH Clinical ADT Group']), ('pos_id', '=', cls.pos_id)])[0]
-
-        cls.patient_id = cls.patient_ids[0]
-        cls.patient_id2 = cls.patient_ids[1]
         spell_data = {
-            'patient_id': cls.patient_id,
-            'pos_id': cls.pos_id,
+            'patient_id': self.patient_id,
+            'pos_id': self.pos_id,
             'code': '1234',
             'start_date': dt.now().strftime(dtf)}
         spell2_data = {
-            'patient_id': cls.patient_id2,
-            'pos_id': cls.pos_id,
+            'patient_id': self.patient_id2,
+            'pos_id': self.pos_id,
             'code': 'abcd',
             'start_date': dt.now().strftime(dtf)}
 
-        spell_activity_id = cls.spell_pool.create_activity(cr, uid, {}, spell_data)
-        cls.activity_pool.start(cr, uid, spell_activity_id)
-        cls.spell_id = spell_activity_id
-        spell_activity_id = cls.spell_pool.create_activity(cr, uid, {}, spell2_data)
-        cls.activity_pool.start(cr, uid, spell_activity_id)
-        cls.spell2_id = spell_activity_id
-
-    def test_complete_and_refresh_ews0_ews1_ews2_from_wardboard(self):
-        cr, uid = self.cr, self.uid
-        ews_data = {
+        spell_activity_id = self.spell_pool.create_activity(cr, uid, {}, spell_data)
+        self.activity_pool.start(cr, uid, spell_activity_id)
+        self.spell_id = spell_activity_id
+        spell_activity_id = self.spell_pool.create_activity(cr, uid, {}, spell2_data)
+        self.activity_pool.start(cr, uid, spell_activity_id)
+        self.spell2_id = spell_activity_id
+        self.ews_data = {
             'respiration_rate': 40,
             'indirect_oxymetry_spo2': 99,
             'oxygen_administration_flag': False,
             'body_temperature': 37.0,
             'blood_pressure_systolic': 120,
             'blood_pressure_diastolic': 80,
-            'pulse_rate': 55,
-            'avpu_text': 'A'
+            'pulse_rate': 55
+        }
+        self.ews_data_2 = {
+            'respiration_rate': 59,
+            'indirect_oxymetry_spo2': 100,
+            'oxygen_administration_flag': False,
+            'body_temperature': 44.9,
+            'blood_pressure_systolic': 300,
+            'blood_pressure_diastolic': 280,
+            'pulse_rate': 250,
+            'avpu_text': 'U'
         }
 
-        cr.execute("""SELECT * FROM ews0""")
-        self.assertEquals([], cr.fetchall())
+    def test_complete_refreshes_ews0_ews1_ews2(self):
+        cr, uid = self.cr, self.uid
 
+        # first observation
         ews_activity_id = self.ews_pool.create_activity(cr, uid, {'parent_id': self.spell_id},
                                                         {'patient_id': self.patient_id})
-        self.ews_pool.submit(cr, uid, ews_activity_id, ews_data)
+        self.ews_pool.submit(cr, uid, ews_activity_id, self.ews_data)
         result = self.ews_pool.complete(cr, uid, ews_activity_id)
         self.assertEquals(result, True)
 
@@ -109,21 +106,11 @@ class TestClinicalPatientObservationEwsRefresh(SingleTransactionCase):
         cr.execute("""SELECT * FROM ews2""")
         self.assertEquals([], cr.fetchall())
 
-        ews_data = {
-            'respiration_rate': 59,
-            'indirect_oxymetry_spo2': 100,
-            'oxygen_administration_flag': False,
-            'body_temperature': 44.9,
-            'blood_pressure_systolic': 300,
-            'blood_pressure_diastolic': 280,
-            'pulse_rate': 250,
-            'avpu_text': 'U'
-        }
-
+        # second observation
         triggered_ews_id = self.activity_pool.search(cr, uid, [
             ['creator_id', '=', ews_activity_id],
             ['data_model', '=', 'nh.clinical.patient.observation.ews']])
-        self.ews_pool.submit(cr, uid, triggered_ews_id[0], ews_data)
+        self.ews_pool.submit(cr, uid, triggered_ews_id[0], self.ews_data_2)
         result = self.ews_pool.complete(cr, uid, triggered_ews_id[0])
         self.assertEquals(result, True)
 
@@ -142,23 +129,100 @@ class TestClinicalPatientObservationEwsRefresh(SingleTransactionCase):
         # activity_rank
         self.assertEquals(1, rows[0][10])
 
-    def test_nh_clinical_wardboard_is_updated(self):
+    def test_complete_refreshes_nh_clinical_wardboard_is_updated(self):
         cr, uid = self.cr, self.uid
 
-        cr.execute("""SELECT * FROM nh_clinical_wardboard""")
-        wb_rows = cr.fetchall()
+        # first observation
+        ews_activity_id = self.ews_pool.create_activity(cr, uid, {'parent_id': self.spell_id},
+                                                        {'patient_id': self.patient_id})
+        self.ews_pool.submit(cr, uid, ews_activity_id, self.ews_data)
+        self.ews_pool.complete(cr, uid, ews_activity_id)
+
+        # second observation
+        triggered_ews_id = self.activity_pool.search(cr, uid, [
+            ['creator_id', '=', ews_activity_id],
+            ['data_model', '=', 'nh.clinical.patient.observation.ews']])
+        self.ews_pool.submit(cr, uid, triggered_ews_id[0], self.ews_data_2)
+        self.ews_pool.complete(cr, uid, triggered_ews_id[0])
+
         cr.execute("""SELECT * FROM ews1""")
         ews1_rows = cr.fetchall()
 
+        cr.execute("""SELECT * FROM nh_clinical_wardboard""")
+        wb_rows = cr.fetchall()
+
         # test for correct patient
-        self.assertEquals(wb_rows[0][1], self.patient_id)
+        self.assertEquals(True, self.patient_id in (row[1] for row in wb_rows))
+        row = [row for row in wb_rows if row[1] == self.patient_id][0]
         # spell_code
-        self.assertEquals(wb_rows[0][6], '1234')
+        self.assertEquals('1234', row[6])
         # ews_score (should be equal to ews1.score)
         ews1_score = str(ews1_rows[0][5])
-        self.assertEquals(wb_rows[0][23], ews1_score)
+        self.assertEquals(row[23], ews1_score)
         # ews_trend_string
-        self.assertEquals(wb_rows[0][25], 'up')
+        self.assertEquals(row[25], 'up')
         # clinical_risk (should be equal to ews1.clinical_risk)
         ews1_clinical_risk = ews1_rows[0][7]
-        self.assertEquals(wb_rows[0][26], ews1_clinical_risk)
+        self.assertEquals(row[26], ews1_clinical_risk)
+
+    def test_create_refreshes_ward_locations(self):
+        cr, uid = self.cr, self.uid
+
+        cr.execute("""SELECT * FROM ward_locations""")
+        rows = cr.fetchall()
+
+        context_id = self.context_pool.search(cr, uid, [['name', '=', 'eobs']])
+        # create new location and refresh materialized view
+        bed_id = self.location_pool.create(cr, uid, {
+                    'name': 'test_bed', 'parent_id': self.wu_id,
+                    'usage': 'bed', 'context_ids': [[6, False, context_id]]})
+
+        location_ids = (row[0] for row in rows)
+        self.assertEquals(False, bed_id in location_ids)
+
+        cr.execute("""SELECT * FROM ward_locations""")
+        rows = cr.fetchall()
+        location_ids = (row[0] for row in rows)
+        self.assertEquals(True, bed_id in location_ids)
+
+    def test_complete_refreshes_placement(self):
+        cr, uid = self.cr, self.uid
+
+        activity_ids = self.activity_pool.search(cr, uid, [
+            ('data_model', '=', 'nh.clinical.spell'),
+            ('patient_id', '=', self.patient_id)])
+        place_id = self.placement_pool.create_activity(cr, uid, {'parent_id': activity_ids[0],
+                                                                 'patient_id': self.patient_id})
+        bed_id = self.location_pool.search(cr, uid, [
+                     ['parent_id', '=', self.wu_id], ['is_available', '=', True], ['usage', '=', 'bed']])
+        self.placement_pool.submit(cr, uid, place_id, {'location_id': bed_id[0], 'patient_id': self.patient_id,
+                                                    'suggested_location_id': self.wu_id})
+        self.placement_pool.complete(cr, uid, place_id)
+
+        cr.execute("""SELECT * FROM placement """)
+        rows = cr.fetchall()
+        self.assertEquals(len(rows), 1)
+        self.assertEquals('completed', rows[0][2])
+        self.assertEquals(rows[0][0], self.patient_id)
+
+    def test_complete_refreshes_param(self):
+        cr, uid = self.cr, self.uid
+
+        # first observation
+        mrsa_activity_id = self.mrsa_pool.create_activity(cr, uid, {'parent_id': self.spell_id},
+                                                        {'patient_id': self.patient_id})
+        self.mrsa_pool.submit(cr, uid, mrsa_activity_id, {'mrsa': True})
+        self.mrsa_pool.complete(cr, uid, mrsa_activity_id)
+        cr.execute("""SELECT * FROM param""")
+        rows = cr.fetchall()
+        self.assertEquals(rows[0][3], True)
+
+        # second observation
+        diabetes_activity_id = self.diabetes_pool.create_activity(cr, uid, {'parent_id': self.spell_id},
+                                                        {'patient_id': self.patient_id})
+        self.diabetes_pool.submit(cr, uid, diabetes_activity_id, {'diabetes': False})
+        self.activity_pool.complete(cr, uid, diabetes_activity_id)
+        cr.execute("""SELECT * FROM param""")
+        rows = cr.fetchall()
+        self.assertEquals(rows[0][2], False)
+
