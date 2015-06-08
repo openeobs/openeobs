c14bd1d3cf5be7b27d88e8020fb692e73636618b
joelortiz <joelortiz@neovahealth.co.uk>
2015-06-08 10:49:35 +0100

    [IMP][WI-678] Neurovascular observations

---
+++ nh_neurovascular/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'NH Clinical Neurovascular Observations',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Neova Health',
+    'website': 'http://www.neovahealth.co.uk/',
+    'depends': ['nh_observations'],
+    'data': ['security/ir.model.access.csv'],
+    'demo': [],
+    'css': [],
+    'js': [],
+    'qweb': [],
+    'images': [],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
---
+++ nh_neurovascular/neurovascular.py
@@ -0,0 +1,58 @@
+from openerp.osv import orm, fields, osv
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class nh_clinical_patient_observation_neurovascular(orm.Model):
+    _name = 'nh.clinical.patient.observation.neurovascular'
+    _inherit = ['nh.clinical.patient.observation']
+    _required = []
+    _description = "Neurovascular Observation"
+    _columns = {
+        'limb': fields.selection([['la', 'Left Arm'], ['ra', 'Right Arm'], ['ll', 'Left Leg'], ['rl', 'Right Leg']], 'Limb'),
+        'colour': fields.char('Colour', size=20),
+        'warmth': fields.char('Warmth', size=20),
+        'movement': fields.char('Movement', size=50),
+        'sensation': fields.char('Sensation', size=50),
+        'pulse': fields.char('Pulse', size=50)
+    }
+    _form_description = [
+        {
+            'name': 'limb',
+            'type': 'selection',
+            'label': 'Limb',
+            'selection': [['la', 'Left Arm'], ['ra', 'Right Arm'], ['ll', 'Left Leg'], ['rl', 'Right Leg']],
+            'initially_hidden': False
+        },
+        {
+            'name': 'colour',
+            'type': 'text',
+            'label': 'Colour',
+            'initially_hidden': False
+        },
+        {
+            'name': 'warmth',
+            'type': 'text',
+            'label': 'Warmth',
+            'initially_hidden': False
+        },
+        {
+            'name': 'movement',
+            'type': 'text',
+            'label': 'Movement',
+            'initially_hidden': False
+        },
+        {
+            'name': 'sensation',
+            'type': 'text',
+            'label': 'Sensation',
+            'initially_hidden': False
+        },
+        {
+            'name': 'pulse',
+            'type': 'text',
+            'label': 'Pulse',
+            'initially_hidden': False
+        }
+    ]
