0b9e6cf5185510c067d435def12a112b979f8c7d
joelortiz <joelortiz@neovahealth.co.uk>
2015-06-08 13:53:16 +0100

    [IMP] extra option for urinary analysis fields

--- nh_urinary_analysis/urinary_analysis.py
+++ nh_urinary_analysis/urinary_analysis.py
@@ -10,7 +10,7 @@ class nh_clinical_patient_observation_urinary_analysis(orm.Model):
     _required = []
     _description = "Urinary Analysis Observation"
     _num_fields = ['ph']
-    _values = [['neg', 'NEG'], ['trace', 'TRACE'], ['1', '+'], ['2', '++'], ['3', '+++']]
+    _values = [['neg', 'NEG'], ['trace', 'TRACE'], ['1', '+'], ['2', '++'], ['3', '+++'], ['4', '++++']]
     _columns = {
         'ph': fields.float('pH', digits=(2, 1)),
         'blood': fields.selection(_values, 'Blood'),
