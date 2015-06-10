f25c8faabd24e6a9e9c121542816d38dbc480c5f
willearp <will@neovahealth.co.uk>
2015-06-10 17:50:01 +0100

    [IMP] [WI-670] created tests for task list and correct template.xml

--- nh_eobs_mobile/tests/test_task_patient_list.py
+++ nh_eobs_mobile/tests/test_task_patient_list.py
@@ -12,17 +12,14 @@ class TaskPatientListTest(NHMobileCommonTest):
         self.nurse = self.demo_data[0]['users']['nurse']
         self.nurse2 = self.demo_data[1]['users']['nurse']
 
-    def test_nh_eobs_mobile_base(self):
-        # test head contains correct URLs
-        # self.assertIn(helpers.URLS['manifest'], get_patients_html)
-        pass
-
     def test_tasklist_when_patient(self):
         cr, uid = self.cr, self.uid
 
-        patients = self.api.get_patients(cr, self.nurse['id'], [], context=self.context)
+        patients = self.api.get_patients(cr, self.nurse['id'], [],
+                                         context=self.context)
         for patient in patients:
-            patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'], patient['id'])
+            patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'],
+                                             patient['id'])
             patient['color'] = 'level-one'
             patient['trend_icon'] = 'icon-{0}-arrow'.format(patient['ews_trend'])
             patient['deadline_time'] = patient['next_ews_time']
@@ -56,16 +53,15 @@ class TaskPatientListTest(NHMobileCommonTest):
         self.assertIn('<i class="icon-alert"></i>', get_patients_html)
         # test javascript element
         self.assertIn("var patient_list", get_patients_html)
-        # test user is included in footer block
-        user = '<p class="user">{0}</p>'.format(self.nurse['login'])
-        self.assertIn(user, get_patients_html)
 
     def test_tasklist_when_patient_and_no_followers_summaries_notifications(self):
         cr, uid = self.cr, self.uid
 
-        patients = self.api.get_patients(cr, self.nurse['id'], [], context=self.context)
+        patients = self.api.get_patients(cr, self.nurse['id'], [],
+                                         context=self.context)
         for patient in patients:
-            patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'], patient['id'])
+            patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'],
+                                             patient['id'])
             patient['color'] = 'level-one'
             patient['trend_icon'] = 'icon-{0}-arrow'.format(patient['ews_trend'])
             patient['deadline_time'] = patient['next_ews_time']
@@ -86,44 +82,94 @@ class TaskPatientListTest(NHMobileCommonTest):
         # test for that no 'summary' is displayed
         self.assertIn('<p class="taskInfo"><br/></p>', get_patients_html)
 
-    def test_task_list(self):
+    def test_tasklist_when_task(self):
         cr, uid = self.cr, self.uid
 
-        # Call controller
-        tasks = self.api.get_activities(cr, self.nurse['id'], [], context=self.context)
+        tasks = self.api.get_activities(cr, self.nurse['id'], [],
+                                        context=self.context)
         for task in tasks:
-            task['url'] = '{0}{1}'.format(helpers.URLS['single_task'], task['id'])
+            task['url'] = '{0}{1}'.format(helpers.URLS['single_task'],
+                                          task['id'])
             task['color'] = 'level-one'
             task['trend_icon'] = 'icon-{0}-arrow'.format(task['ews_trend'])
-            task['summary'] = task['summary'] if task.get('summary') else False
-            task['notification_string'] = '<i class="icon-alert"></i>' if task['notification'] == True else ''
+            task['summary'] = "This is a summary for %s" % task['id']
+            task['notification'] = True
+            task['follower_ids'] = [self.nurse['id'], self.nurse2['id']]
 
-        get_task_html = self.render_template(cr, uid, 'nh_eobs_mobile.patient_task_list',
-                                              {'items': tasks,
-                                               'section': 'task',
-                                               'username': self.nurse['display_name'],
-                                               'urls': helpers.URLS})
+        get_task_html = self.render_template(
+            cr, uid, 'nh_eobs_mobile.patient_task_list',
+            {'items': tasks, 'section': 'task',
+             'username': self.nurse['display_name'],
+             'urls': helpers.URLS, 'user_id': self.nurse['id']})
 
+        # when "section==task"
+        self.assertIn(helpers.URLS['task_list'], get_task_html)
+        self.assertIn(helpers.URLS['patient_list'], get_task_html)
+
+        for task in tasks:
+            self.assertIn(task['url'], get_task_html)
+            self.assertIn(task['color'], get_task_html)
+            self.assertIn(task['trend_icon'], get_task_html)
+            self.assertIn(task['location'], get_task_html)
+            self.assertIn(task['parent_location'], get_task_html)
+            self.assertIn(task['summary'], get_task_html)
+
+        # test 'follower_ids'
+        self.assertIn('Claim', get_task_html)
+        # test 'notification'
+        self.assertIn('<i class="icon-alert"></i>', get_task_html)
+        # test javascript element
+        self.assertIn("var patient_list", get_task_html)
+
+    def test_tasklist_when_task_and_no_followers_summaries_notifications(self):
+        cr, uid = self.cr, self.uid
+
+        tasks = self.api.get_activities(cr, self.nurse['id'], [],
+                                        context=self.context)
+        for task in tasks:
+            task['url'] = '{0}{1}'.format(helpers.URLS['single_task'],
+                                          task['id'])
+            task['color'] = 'level-one'
+            task['trend_icon'] = 'icon-{0}-arrow'.format(task['ews_trend'])
+            task['summary'] = False
+            task['notification'] = False
+            task['follower_ids'] = [self.nurse2['id']]
+
+        get_task_html = self.render_template(
+            cr, uid, 'nh_eobs_mobile.patient_task_list',
+            {'items': tasks, 'section': 'task',
+             'username': self.nurse['display_name'],
+             'urls': helpers.URLS, 'user_id': self.nurse['id']})
+
+        # test 'follower_ids'
+        self.assertNotIn('<p class="aside">Claim</p>', get_task_html)
+        # test no 'notification' icon is displayed
+        self.assertNotIn('<i class="icon-alert"></i>', get_task_html)
+        # test for that no 'summary' is displayed
+        self.assertIn('<p class="taskInfo"><br/></p>', get_task_html)
 
     def test_followed_patients(self):
         cr, uid = self.cr, self.uid
 
-        # find patients to follow
-        ptf = self.api.get_patients(cr, self.nurse2['id'], [], context=self.context)
-        patient_ids = [p['id'] for p in ptf]
-        # assign patients to this user
-        follow_activity_id = self.api.follow_invite(cr, self.nurse2['id'], patient_ids, self.nurse['id'],
+        patients_to_follow = self.api.get_patients(cr, self.nurse2['id'], [],
+                                                   context=self.context)
+        patient_ids = [patient['id'] for patient in patients_to_follow]
+        follow_activity_id = self.api.follow_invite(cr, self.nurse2['id'],
+                                                    patient_ids,
+                                                    self.nurse['id'],
                                                     context=self.context)
-        self.api.complete(cr, self.nurse['id'], follow_activity_id, {}, context=self.context)
-        # get patient list
-        patients = self.api.get_patients(cr, self.nurse['id'], [], context=self.context)
+        self.api.complete(cr, self.nurse['id'], follow_activity_id, {},
+                          context=self.context)
+
+        patients = self.api.get_patients(cr, self.nurse['id'], [],
+                                         context=self.context)
         for patient in patients:
             patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'], patient['id'])
             patient['color'] = 'level-one'
             patient['trend_icon'] = 'icon-{0}-arrow'.format(patient['ews_trend'])
             patient['deadline_time'] = patient['next_ews_time']
             patient['summary'] = patient['summary'] if patient.get('summary') else False
-        # get followed patient list
+
         followed_patients = self.api.get_followed_patients(cr, self.nurse['id'], context=self.context)
         for patient in followed_patients:
             patient['url'] = '{0}{1}'.format(helpers.URLS['single_patient'], patient['id'])
@@ -131,40 +177,17 @@ class TaskPatientListTest(NHMobileCommonTest):
             patient['trend_icon'] = 'icon-{0}-arrow'.format(patient['ews_trend'])
             patient['deadline_time'] = patient['next_ews_time']
             patient['summary'] = patient['summary'] if patient.get('summary') else False
-        # call controller
-        get_patients_html = self.render_template(cr, uid, 'nh_eobs_mobile.patient_task_list', {'items': patients,
-                                                                                               'followed_items': followed_patients,
-                                                                                               'section': 'patient',
-                                                                                               'username': self.nurse[
-                                                                                                   'display_name'],
-                                                                                               'urls': helpers.URLS})
-        # create test DOm
-        patient_list_string = ""
-        for patient in patients:
-            patient_list_string += helpers.LIST_ITEM.format(url=patient['url'],
-                                                            deadline=patient['deadline_time'],
-                                                            full_name=patient['full_name'],
-                                                            ews_score=patient['ews_score'],
-                                                            trend_icon=patient['trend_icon'],
-                                                            location=patient['location'],
-                                                            parent_location=patient['parent_location'],
-                                                            notification='',
-                                                            summary='')
-        patient_list_string += "<li><hr/></li>"
+
+        get_patients_html = self.render_template(
+            cr, uid, 'nh_eobs_mobile.patient_task_list',
+            {'items': patients, 'followed_items': followed_patients,
+             'section': 'patient', 'username': self.nurse['display_name'],
+             'urls': helpers.URLS})
+
         for patient in followed_patients:
-            patient_list_string += helpers.LIST_ITEM.format(url=patient['url'],
-                                                            deadline=patient['deadline_time'],
-                                                            full_name=patient['full_name'],
-                                                            ews_score=patient['ews_score'],
-                                                            trend_icon=patient['trend_icon'],
-                                                            location=patient['location'],
-                                                            parent_location=patient['parent_location'],
-                                                            notification='',
-                                                            summary='')
-        example_html = helpers.BASE_HTML.format(content='<ul class="tasklist">{0}</ul>'.format(patient_list_string),
-                                                patient_selected=' class="selected"', task_selected='',
-                                                user=self.nurse['display_name'])
-        # assert
-        self.assertTrue(self.compare_doms(get_patients_html, example_html),
-                        'DOM from Controller ain\'t the same as DOM from example')
-        # profit
+            self.assertIn(patient['url'], get_patients_html)
+            self.assertIn(patient['deadline_time'], get_patients_html)
+            self.assertIn(patient['location'], get_patients_html)
+            self.assertIn(patient['parent_location'], get_patients_html)
+            self.assertIn(patient['color'], get_patients_html)
+
