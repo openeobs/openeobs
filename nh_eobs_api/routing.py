ddb2d5ce27b1f9a7a6f6325bb95212ac38d7ef3f
Lorenzo Pascucci <lorenzo@neovahealth.co.uk>
2015-07-10 17:15:49 +0100

    Improve the 'expose_route' method of Route Manager
    Now the 'url_prefix' parameter gives priority to the value passed when calling the method,
    instead of the value set when instantiating the Route Manager.

--- nh_eobs_api/routing.py
+++ nh_eobs_api/routing.py
@@ -80,7 +80,7 @@ class RouteManager(object):
         :raise: KeyError (if no Route object matching the 'route_name' parameter is found)
         """
         route = self.ROUTES[route_name]
-        route_prefix = self.URL_PREFIX if self.URL_PREFIX else url_prefix
+        route_prefix = url_prefix or self.URL_PREFIX
         if route:
             route_dict = {
                 'route': str(route_prefix) + route.url if route_prefix else route.url,
@@ -102,9 +102,10 @@ class RouteManager(object):
         :raise: KeyError (if no Route object matching the 'route_name' parameter is found)
         """
         route = self.ROUTES[route_name]
+        route_prefix = url_prefix or self.URL_PREFIX
         if route:
             route_dict = {  # TODO: exclude 'falsy' parameters ?
-                'route': str(url_prefix) + route.url if url_prefix else route.url,
+                'route': str(route_prefix) + route.url if route_prefix else route.url,
                 'auth': route.auth,
                 'methods': route.methods,
                 'headers': route.headers,
