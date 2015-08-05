5de7dac0e83a9bc5c0a726592a1095ae9bbdb012
joelortiz <joelortiz@neovahealth.co.uk>
2015-08-05 15:01:14 +0100

    [FIX] rollback 2 stupid changes

--- nh_eobs/tests/__init__.py
+++ nh_eobs/tests/__init__.py
@@ -1,7 +1,7 @@
-# import test_api
-# import test_api_demo
-# import test_observation_extension
-# import test_observation_report
-# import test_observation_report_data_dict
-# import test_observation_report_create_filter
+import test_api
+import test_api_demo
+import test_observation_extension
+import test_observation_report
+import test_observation_report_data_dict
+import test_observation_report_create_filter
 import test_wardboard
