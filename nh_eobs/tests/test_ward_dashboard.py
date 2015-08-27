c9bb102e95558f93ebaff0ee9ffd07a2fbe76390
joelortiz <joelortiz@neovahealth.co.uk>
2015-08-27 15:23:29 +0100

    [IMP][WI-1028] Ward Dashboard Patients Board button test

--- nh_eobs/tests/test_ward_dashboard.py
+++ nh_eobs/tests/test_ward_dashboard.py
@@ -171,3 +171,19 @@ class TestWardDashboard(SingleTransactionCase):
         self.assertEqual(ward.low_risk_patients, 0, msg='Incorrect low_risk_patients field')
         self.assertEqual(ward.no_risk_patients, 0, msg='Incorrect no_risk_patients field')
         self.assertEqual(ward.noscore_patients, 3, msg='Incorrect noscore_patients field')
+
+    def test_10_ward_dashboard_patient_board(self):
+        cr, uid = self.cr, self.uid
+
+        res = self.ward_pool.patient_board(cr, self.wm_uid, [self.ward_id], {})
+        self.assertDictEqual(res, {
+            'name': 'Patients Board',
+            'type': 'ir.actions.act_window',
+            'res_model': 'nh.clinical.wardboard',
+            'view_type': 'form',
+            'view_mode': 'kanban,form,tree',
+            'domain': [('spell_state', '=', 'started'), ('location_id.usage', '=', 'bed')],
+            'target': 'current',
+            'context': {'search_default_clinical_risk': 1, 'search_default_high_risk': 0,
+                        'search_default_ward_id': self.ward_id}
+        })
