c5cbc51a9e9738b3a161c5328f8b73731e894f15
Colin Wren <colin@neovahealth.co.uk>
2015-09-29 14:39:51 +0100

    [WI-675] Adding Styleguide resources to use with KSS

--- nh_eobs_mobile/static/dev/less/gulpfile.js
+++ nh_eobs_mobile/static/dev/less/gulpfile.js
@@ -1,9 +1,27 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),
-	util = require('gulp-util');
+	util = require('gulp-util'),
+	del = require('del'),
+	kss = require('gulp-kss');
 
 gulp.task('compileLESS', function(){
 	gulp.src('src/compile.less')
 	.pipe(less().on('error', util.log))
 	.pipe(gulp.dest('style/'));
 });
+
+gulp.task('Styleguide', function(){
+	del(['styleguide/**']);
+	gulp.src(['src/*.less'])
+	.pipe(kss({
+		overview: 'src/styleguide_overview.md',
+		templateDirectory: 'src/templates'
+	}))
+	.pipe(gulp.dest('styleguide/'));
+
+	gulp.src(['src/compile.less'])
+	.pipe(less().on('error', util.log))
+	.pipe(gulp.dest('styleguide/public'))
+});
+
+gulp.task('default', ['compileLESS'])
