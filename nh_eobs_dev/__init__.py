b53c264553c3b9e15be84916a8fdac3ecdd16dd9
Colin Wren <colin@neovahealth.co.uk>
2015-11-26 14:16:49 +0000

    [WI-1717][Travis] Removing Dependency cycle on Patient Flow

--- nh_eobs_dev/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Open eObs. See LICENSE file for full copyright and licensing details.
-# import websockets
-import controllers
--- nh_eobs_dev/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Open eObs. See LICENSE file for full copyright and licensing details.
-__author__ = 'colin'
-import main
