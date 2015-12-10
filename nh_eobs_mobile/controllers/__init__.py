92c2bdc521b45dba313b63f6ef030bc81d244feb
Colin Wren <colin@neovahealth.co.uk>
2015-12-10 17:24:13 +0000

    [IMP][WI-1759][Open eObs linting] urls bug fixed

--- nh_eobs_mobile/controllers/__init__.py
+++ nh_eobs_mobile/controllers/__init__.py
@@ -1,2 +1,3 @@
 # Part of Open eObs. See LICENSE file for full copyright and licensing details.
 import openerp.addons.nh_eobs_mobile.controllers.main
+import openerp.addons.nh_eobs_mobile.controllers.urls
