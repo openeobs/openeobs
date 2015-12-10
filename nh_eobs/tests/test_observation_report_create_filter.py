75e3cc0c6699970e2c20381df7f858b5d78ca2b1
Colin Wren <colin@gimpneek.com>
2015-12-10 23:45:22 +0000

    [IMP][WI-1759][Open eObs linting] Obs report tests

--- nh_eobs/tests/test_observation_report_create_filter.py
+++ nh_eobs/tests/test_observation_report_create_filter.py
@@ -1,12 +1,13 @@
 # Part of Open eObs. See LICENSE file for full copyright and licensing details.
-__author__ = 'colinwren'
 from openerp.tests.common import TransactionCase
 from datetime import datetime, timedelta
-import logging
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as dtf
-from openerp.addons.nh_eobs.report.print_observation_report import ObservationReport as obs_report
+from openerp.addons.nh_eobs.report.print_observation_report import \
+    ObservationReport as obs_report
+import logging
 
 _logger = logging.getLogger(__name__)
+
 
 class TestObservationReport(TransactionCase):
 
@@ -19,114 +20,237 @@ class TestObservationReport(TransactionCase):
         self.start_date = datetime.now() + timedelta(days=5)
         self.end_date = datetime.now() + timedelta(days=5)
 
-    def test_01_create_search_filter_with_normal_model_with_end_date_with_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.normal_model, self.start_date, self.end_date)
-        self.assertEqual(len(test_filter), 5, 'Incorrect number of items in filter')
+    def test_01_with_normal_model_with_end_date_with_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.normal_model, self.start_date, self.end_date)
+        self.assertEqual(len(test_filter), 5,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         complete_filter = test_filter[2]
         start_filter = test_filter[3]
         end_filter = test_filter[4]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
-        self.assertEqual(str(complete_filter), "['state', '=', 'completed']", 'Incorrect complete filter created')
-        self.assertEqual(str(start_filter), "['date_started', '>=', '{0}']".format(datetime.strftime(self.start_date, dtf)), 'Incorrect date start filter created')
-        self.assertEqual(str(end_filter), "['date_terminated', '<=', '{0}']".format(datetime.strftime(self.end_date, dtf)), 'Incorrect date end filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created'
+        )
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', 'nh.test']",
+            'Incorrect model filter created'
+        )
+        self.assertEqual(
+            str(complete_filter),
+            "['state', '=', 'completed']",
+            'Incorrect complete filter created'
+        )
+        self.assertEqual(
+            str(start_filter),
+            "['date_started', '>=', '{0}']".format(
+                datetime.strftime(self.start_date, dtf)
+            ),
+            'Incorrect date start filter created'
+        )
+        self.assertEqual(
+            str(end_filter),
+            "['date_terminated', '<=', '{0}']".format(
+                datetime.strftime(self.end_date, dtf)
+            ),
+            'Incorrect date end filter created'
+        )
 
-    def test_02_create_search_filter_with_exception_model_with_end_date_with_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.exception_model, self.start_date, self.end_date)
-        self.assertEqual(len(test_filter), 4, 'Incorrect number of items in filter')
+    def test_02_with_exception_model_with_end_date_with_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.exception_model,
+            self.start_date, self.end_date)
+        self.assertEqual(
+            len(test_filter),
+            4,
+            'Incorrect number of items in filter'
+        )
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         start_filter = test_filter[2]
         end_filter = test_filter[3]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', '{0}']".format(self.exception_model), 'Incorrect model filter created')
-        self.assertEqual(str(start_filter), "['date_started', '>=', '{0}']".format(datetime.strftime(self.start_date, dtf)), 'Incorrect date start filter created')
-        self.assertEqual(str(end_filter), "['date_terminated', '<=', '{0}']".format(datetime.strftime(self.end_date, dtf)), 'Incorrect date end filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created'
+        )
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', '{0}']".format(
+                self.exception_model
+            ), 'Incorrect model filter created')
+        self.assertEqual(
+            str(start_filter),
+            "['date_started', '>=', '{0}']".format(
+                datetime.strftime(self.start_date, dtf)
+            ), 'Incorrect date start filter created')
+        self.assertEqual(
+            str(end_filter),
+            "['date_terminated', '<=', '{0}']".format(
+                datetime.strftime(self.end_date, dtf)
+            ), 'Incorrect date end filter created')
 
-    def test_03_create_search_filter_without_model_with_end_date_with_start_date(self):
+    def test_03_without_model_with_end_date_with_start_date(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(None, self.normal_model, self.start_date, self.end_date)
+            test_filter = obs_report.create_search_filter(
+                None, self.normal_model, self.start_date, self.end_date)
 
-    def test_04_create_search_filter_with_normal_model_without_end_date_with_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.normal_model, self.start_date, None)
-        self.assertEqual(len(test_filter), 4, 'Incorrect number of items in filter')
+    def test_04_with_normal_model_without_end_date_with_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.normal_model, self.start_date, None)
+        self.assertEqual(
+            len(test_filter), 4, 'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         complete_filter = test_filter[2]
         start_filter = test_filter[3]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
-        self.assertEqual(str(complete_filter), "['state', '=', 'completed']", 'Incorrect complete filter created')
-        self.assertEqual(str(start_filter), "['date_started', '>=', '{0}']".format(datetime.strftime(self.start_date, dtf)), 'Incorrect date start filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
+        self.assertEqual(
+            str(complete_filter),
+            "['state', '=', 'completed']", 'Incorrect complete filter created')
+        self.assertEqual(
+            str(start_filter),
+            "['date_started', '>=', '{0}']".format(
+                datetime.strftime(self.start_date, dtf)
+            ), 'Incorrect date start filter created')
 
-    def test_05_create_search_filter_with_normal_model_without_end_date_without_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.normal_model, None, None)
-        self.assertEqual(len(test_filter), 3, 'Incorrect number of items in filter')
+    def test_05_with_normal_model_without_end_date_without_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.normal_model, None, None)
+        self.assertEqual(len(test_filter), 3,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         complete_filter = test_filter[2]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
-        self.assertEqual(str(complete_filter), "['state', '=', 'completed']", 'Incorrect complete filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
+        self.assertEqual(
+            str(complete_filter),
+            "['state', '=', 'completed']", 'Incorrect complete filter created')
 
-    def test_06_create_search_filter_with_normal_model_with_end_date_without_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.normal_model, None, self.end_date)
-        self.assertEqual(len(test_filter), 4, 'Incorrect number of items in filter')
+    def test_06_with_normal_model_with_end_date_without_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.normal_model, None, self.end_date)
+        self.assertEqual(len(test_filter), 4,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         complete_filter = test_filter[2]
         end_filter = test_filter[3]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', 'nh.test']", 'Incorrect model filter created')
-        self.assertEqual(str(complete_filter), "['state', '=', 'completed']", 'Incorrect complete filter created')
-        self.assertEqual(str(end_filter), "['date_terminated', '<=', '{0}']".format(datetime.strftime(self.end_date, dtf)), 'Incorrect date start filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', 'nh.test']",
+            'Incorrect model filter created')
+        self.assertEqual(
+            str(complete_filter),
+            "['state', '=', 'completed']",
+            'Incorrect complete filter created')
+        self.assertEqual(
+            str(end_filter),
+            "['date_terminated', '<=', '{0}']".format(
+                datetime.strftime(self.end_date, dtf)
+            ), 'Incorrect date start filter created')
 
-    def test_07_create_search_filter_with_exception_model_with_end_date_without_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.exception_model, None, self.end_date)
-        self.assertEqual(len(test_filter), 3, 'Incorrect number of items in filter')
+    def test_07_with_exception_model_with_end_date_without_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.exception_model, None, self.end_date)
+        self.assertEqual(len(test_filter), 3,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         end_filter = test_filter[2]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', '{0}']".format(self.exception_model), 'Incorrect model filter created')
-        self.assertEqual(str(end_filter), "['date_terminated', '<=', '{0}']".format(datetime.strftime(self.end_date, dtf)), 'Incorrect date start filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', '{0}']".format(
+                self.exception_model), 'Incorrect model filter created')
+        self.assertEqual(
+            str(end_filter),
+            "['date_terminated', '<=', '{0}']".format(
+                datetime.strftime(self.end_date, dtf)
+            ), 'Incorrect date start filter created')
 
-    def test_08_create_search_filter_with_exception_model_without_end_date_with_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.exception_model2, self.start_date, None)
-        self.assertEqual(len(test_filter), 3, 'Incorrect number of items in filter')
+    def test_08_with_exception_model_without_end_date_with_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.exception_model2, self.start_date, None)
+        self.assertEqual(len(test_filter), 3,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
         start_filter = test_filter[2]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', '{0}']".format(self.exception_model2), 'Incorrect model filter created')
-        self.assertEqual(str(start_filter), "['date_started', '>=', '{0}']".format(datetime.strftime(self.start_date, dtf)), 'Incorrect date start filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', '{0}']".format(
+                self.exception_model2), 'Incorrect model filter created')
+        self.assertEqual(
+            str(start_filter),
+            "['date_started', '>=', '{0}']".format(
+                datetime.strftime(self.start_date, dtf)
+            ), 'Incorrect date start filter created')
 
-    def test_09_create_search_filter_with_exception_model_without_end_date_without_start_date(self):
-        test_filter = obs_report.create_search_filter(self.spell_id, self.exception_model, None, None)
-        self.assertEqual(len(test_filter), 2, 'Incorrect number of items in filter')
+    def test_09_with_exception_model_without_end_date_without_start_date(self):
+        test_filter = obs_report.create_search_filter(
+            self.spell_id, self.exception_model, None, None)
+        self.assertEqual(len(test_filter), 2,
+                         'Incorrect number of items in filter')
         spell_filter = test_filter[0]
         model_filter = test_filter[1]
-        self.assertEqual(str(spell_filter), "['parent_id', '=', 1]", 'Incorrect spell activity filter created')
-        self.assertEqual(str(model_filter), "['data_model', '=', '{0}']".format(self.exception_model), 'Incorrect model filter created')
+        self.assertEqual(
+            str(spell_filter),
+            "['parent_id', '=', 1]",
+            'Incorrect spell activity filter created')
+        self.assertEqual(
+            str(model_filter),
+            "['data_model', '=', '{0}']".format(
+                self.exception_model), 'Incorrect model filter created')
 
-    def test_10_create_search_filter_without_model_without_end_date_with_start_date(self):
+    def test_10_without_model_without_end_date_with_start_date(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(self.spell_id, None, self.start_date, None)
+            test_filter = obs_report.create_search_filter(
+                self.spell_id, None, self.start_date, None)
 
-    def test_11_create_search_filter_without_model_without_end_date_without_start_date(self):
+    def test_11_without_model_without_end_date_without_start_date(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(self.spell_id, None, None, None)
+            test_filter = obs_report.create_search_filter(
+                self.spell_id, None, None, None)
 
-    def test_12_create_search_filter_without_model_with_end_date_without_start_date(self):
+    def test_12_without_model_with_end_date_without_start_date(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(self.spell_id, None, None, self.end_date)
+            test_filter = obs_report.create_search_filter(
+                self.spell_id, None, None, self.end_date)
 
-    def test_13_create_search_filter_without_model_with_end_date_with_start_date(self):
+    def test_13_without_model_with_end_date_with_start_date(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(self.spell_id, None, self.start_date, self.end_date)
+            test_filter = obs_report.create_search_filter(
+                self.spell_id, None, self.start_date, self.end_date)
 
-    def test_14_create_search_filter_without_spell_activity_id(self):
+    def test_14_without_spell_activity_id(self):
         with self.assertRaises(ValueError):
-            test_filter = obs_report.create_search_filter(None, None, None, None)
+            test_filter = obs_report.create_search_filter(
+                None, None, None, None)
