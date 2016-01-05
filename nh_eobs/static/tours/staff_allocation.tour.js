85073370ce8573d0a370f82c2ed9e37cc0c4bb3e
Jon Wyatt <jonwyatt@neovahealth.co.uk>
2016-01-05 10:36:03 +0000

    [WI- 1870] Added steps for staff_reallocation tour.

--- nh_eobs/static/tours/staff_allocation.tour.js
+++ nh_eobs/static/tours/staff_allocation.tour.js
@@ -14,11 +14,11 @@
         steps: [
             {
                 title:     _t("Nursing Staff Shift Change Tutorial"),
-                content: _t("It's 8pm, I am the designated nurse in charge for ward C for the upcoming night shift. I need to assign staff to patients at the beginning of my shift."),
+                content: _t("I am the designated nurse in charge for ward C for the upcoming shift. I need to assign staff to patients at the beginning of my shift."),
                 popover:   { next: _t("Continue"), end: _t("Exit") }
             },
             {
-                title:     _t("Logged in as a ward manager"),
+                title:     _t("Log in as ward manager"),
                 content: _t("Only ward managers and senior managers are able to access the 'Nursing Shift Change' wizard"),
                 element:   ".oe_topbar_name:contains('Winifred')",
                 popover:   { next: _t("Continue"), end: _t("Exit") },
@@ -32,14 +32,15 @@
             {
                 title:     _t("Select Ward"),
                 content: _t("Choose your ward from the drop down menu and click 'Start'"),
-                element:   "button:contains('Start')",
+                element:   ".modal-dialog input:eq(0)",
                 placement: 'top'
             },
             {
                 title:     _t("Previous Shift"),
-                element: "div:contains('Previous Shift')",
-                content: _t("This shows a list of all beds in the selected ward and the responsible staff members"),
-                popover:   { next: _t("Continue"), end: _t("Exit") }
+                element: "li.oe_active span.label:contains('De-allocate')",
+                content: _t("This shows a list of all beds in the selected ward and the currently responsible staff members"),
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'bottom'
             },
             {
                 title:     _t("Click 'Deallocate Previous Shift'"),
@@ -50,15 +51,15 @@
             {
                 title:     _t("Enter staff for next shift"),
                 content: _t("You can add multiple staff members. Begin typing a name to find matches or press down key to view list. Click 'Select' when done"),
-                element:   ".modal-dialog button:contains('Select')", // Add 7 x HCA's and 7 x nurses
-                //element:   ".modal-dialog input", // Add 7 x HCA's and 7 x nurses
+                element:   ".modal-dialog textarea",
                 placement: 'top'
             },
             {
                 title:     _t("Allocation Tab"),
-                element: "div:contains('Previous Shift')",
-                content: _t("This shows a list of all beds requiring staff allocation in this ward"),
-                popover:   { next: _t("Continue"), end: _t("Exit") }
+                element: "li.oe_active span.label:contains('Allocation')",
+                content: _t("This shows all the beds in the ward requiring staff allocation"),
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'bottom'
             },
             {
                 title:     _t("Click Allocate"),
@@ -68,27 +69,28 @@
             },
             {
                 title:     _t("Select Nurse / HCA"),
-                content: _t("Choose from dropdown or begin typing to get suggested names"),
+                content: _t("Choose from dropdown or begin typing to get suggested names. Click 'Continue' when done."),
                 element:   ".modal-dialog:eq(1) input:eq(0)",
                 popover:   { next: _t("Continue"), end: _t("Exit") },
                 placement: 'top'
             },
             {
                 title:     _t("Navigation"),
-                content: _t("You can use the arrows to scroll through the beds in ward. Click here to view the next bed in list."),
+                content: _t("You can use the arrows to scroll through the beds in the ward. Click here to save changes and view the next bed in list."),
                 element:   ".modal-dialog:eq(1) a[data-pager-action='next']:eq(0)",
                 placement: 'bottom'
             },
             {
                 waitFor: ".modal-dialog:eq(1) span:contains('Bed 02')",
                 title:     _t("Save Changes"),
-                content: _t("When finished allocating you can click here to save changes and close popup"),
-                element:   ".modal-dialog:eq(1) button.oe_form_button_save"
+                content: _t("When you've finished allocating, click here to save changes and close the popup"),
+                element:   ".modal-dialog:eq(1) button.oe_form_button_save",
+                placement: 'bottom'
             },
             {
                 waitNot: ".modal-dialog:eq(1)",
                 title:     _t("Confirm Changes"),
-                content: _t("Review your changes here and click to confirm and return to the main page"),
+                content: _t("Review your changes then click here to confirm and return to the main page"),
                 element:   ".modal-dialog span:contains('Confirm Allocation')",
                 placement: 'bottom'
             },
@@ -96,33 +98,85 @@
                 waitNot: ".modal-dialog",
                 title:     _t("Tutorial Completed"),
                 content: _t("That's it. You're staff have now been allocated"),
-                popover:   { end: _t("Exit") }
+                popover:   { next: _t("Exit")}
             }
         ]
     });
-    //openerp.Tour.register({
-    //    id: 'nursing_reallocation',
-    //    name: _t("Re-allocating nursing staff during shift (Winifred)"),
-    //    path: '/web?debug=',
-    //    mode: 'tour',
-    //    steps: [
-    //        {
-    //            title:     _t("Nursing Staff Re-Allocation Tutorial"),
-    //            content: _t("It's 8pm, I am the designated nurse in charge for ward C for the upcoming night shift. I need to assign staff to patients at the beginning of my shift."),
-    //            popover:   { next: _t("Continue"), end: _t("Exit") }
-    //        },
-    //        {
-    //            title:     _t("Logged in as a ward manager"),
-    //            content: _t("Only ward managers and senior managers are able to access the 'Nursing Shift Change' wizard"),
-    //            element:   ".oe_topbar_name:contains('Winifred')",
-    //            popover:   { next: _t("Continue"), end: _t("Exit") },
-    //            placement: 'bottom'
-    //        },
-    //        {
-    //            title:     _t("Click 'Nursing Shift Change'"),
-    //            content: _t("This will open the nursing shift change wizard"),
-    //            element:   "span.oe_menu_text:contains('Nursing Shift Change')"
-    //        },
-    //    ]
-    //});
+    openerp.Tour.register({
+        id: 'nursing_reallocation',
+        name: _t("Re-allocating nursing staff during shift (Winifred)"),
+        path: '/web?debug=',
+        mode: 'tour',
+        steps: [
+            {
+                title:     _t("Nursing Staff Re-Allocation Tutorial"),
+                content: _t("I am designated nurse in charge for ward C. A new nurse has just started his shift. I need to re-allocate beds."),
+                popover:   { next: _t("Continue"), end: _t("Exit") }
+            },
+            {
+                title:     _t("Log in as ward manager"),
+                content: _t("Only ward managers and senior managers are able to access the 'Nursing Re-Allocation' wizard"),
+                element:   ".oe_topbar_name:contains('Winifred')",
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'bottom'
+            },
+            {
+                title:     _t("Click here"),
+                content: _t("This will open the nursing re-allocation wizard"),
+                element:   "span.oe_menu_text:contains('Nursing Re-Allocation')"
+            },
+            {
+                title:     _t("Add / Remove Staff"),
+                content: _t("Remove staff by clicking the 'x' next to their name. Add staff by typing in their name or selecting from the dropdown."),
+                element:   ".modal-dialog textarea",
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'top'
+            },
+            {
+                title:     _t("Confirm Changes"),
+                content: _t("When happy with changes to staff on shift, click here"),
+                element:   "button:contains('Re-Allocate')",
+                placement: 'bottom'
+            },
+            {
+                title:     _t("Allocation Tab"),
+                element: "li.oe_active span.label:contains('Allocation')",
+                content: _t("This shows all the beds in the ward and the staff currently assigned"),
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'bottom'
+            },
+            {
+                title:     _t("Click Here"),
+                content: _t("To re-allocate the unassigned bed to the new member of staff"),
+                element:   ".modal-dialog button[title='Allocate']:eq(0)",
+                placement: 'top'
+            },
+            {
+                title:     _t("Select Nurse"),
+                content: _t("Choose from dropdown or begin typing to get suggested name"),
+                element:   ".modal-dialog:eq(1) input:eq(0)",
+                popover:   { next: _t("Continue"), end: _t("Exit") },
+                placement: 'top'
+            },
+            {
+                title:     _t("Click Here"),
+                content: _t("To confirm changes and close popup"),
+                element:   ".modal-dialog:eq(1) button.oe_form_button_save",
+                placement: 'bottom'
+            },
+            {
+                waitNot: ".modal-dialog:eq(1)",
+                title:     _t("Review and Confirm"),
+                content: _t("Review your changes then click here to confirm and return to the main page"),
+                element:   ".modal-dialog span:contains('Confirm Allocation')",
+                placement: 'bottom'
+            },
+            {
+                waitNot: ".modal-dialog",
+                title:     _t("Tutorial Completed"),
+                content: _t("That's it. You're staff have been successfully allocated."),
+                popover:   { next: _t("Exit")}
+            }
+        ]
+    });
 }());
