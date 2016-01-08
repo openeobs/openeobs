21aaa3c971896e58717ff9b63c89f14942082363
Will Earp <will.earp@icloud.com>
2016-01-08 12:00:15 +0000

    [WI-1880] Added generate news simulation function.

--- nh_eobs_demo/tests/test_api.py
+++ nh_eobs_demo/tests/test_api.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import logging
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from openerp.osv import osv
 from openerp.tests.common import TransactionCase
@@ -21,6 +21,8 @@ class TestApiDemo(TransactionCase):
         self.spell_pool = self.registry('nh.clinical.spell')
         self.demo_loader = self.registry('nh.eobs.demo.loader')
         self.demo_api = self.registry('nh.clinical.api.demo')
+        self.activity_pool = self.registry('nh.activity')
+        self.location_pool = self.registry('nh.clinical.location')
 
         self.adt_uid = self.user_pool.search(
             cr, uid, [('login', '=', 'adt')])[0]
@@ -98,5 +100,27 @@ class TestApiDemo(TransactionCase):
         user_id = self.demo_loader._get_random_user_id(cr, uid, user_ids)
         self.assertTrue(user_id in user_ids)
 
+    def test_generate_news_simulation(self):
+        cr, uid = self.cr, self.uid
 
+        locations = self.demo_api.generate_locations(cr, uid, wards=1, beds=3, hospital=True)
+        location_ids = locations.get('Ward 1')
+        users = self.demo_api.generate_users(cr, uid, location_ids)
+        patient_ids = self.demo_api.generate_patients(cr, users['adt'][0], 3)
+        results = self.location_pool.read(cr, uid, [location_ids[0]], ['code'])
 
+        # generate eobs for 2 days
+        start_date = datetime.now() - timedelta(days=2)
+        data = {'location': results[0]['code'], 'start_date': start_date}
+        admit_patient_ids = self.demo_api.admit_patients(cr, users['adt'][0], patient_ids, data)
+
+        self.demo_api.place_patients(cr, uid, admit_patient_ids, location_ids[0])
+        result = self.demo_loader.generate_news_simulation(
+            cr, uid, 1, patient_ids=admit_patient_ids)
+
+        self.assertEquals(result, True)
+        scheduled_ids = self.activity_pool.search(cr, uid, [
+            ['patient_id', 'in', patient_ids],
+            ['data_model', '=', 'nh.clinical.patient.observation.ews'],
+            ['state', 'not in', ['completed', 'cancelled']]])
+        self.assertEquals(len(scheduled_ids), 3)
