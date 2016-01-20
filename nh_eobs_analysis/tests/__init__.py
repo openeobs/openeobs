5fc4c01684d5b01c9dc6ef6d7a980d63c032d1d3
Lorenzo Pascucci <lorenzo@neovahealth.co.uk>
2016-01-20 17:27:46 +0000

    Add test file related to OLAP view performance

--- nh_eobs_analysis/tests/__init__.py
+++ nh_eobs_analysis/tests/__init__.py
@@ -1,4 +1,6 @@
 # Part of Open eObs. See LICENSE file for full copyright and licensing details.
 # -*- coding: utf-8 -*-
+from . import test_olap_dataset_and_manipulation
 from . import test_olap_view_dimensions
+from . import test_olap_view_performance
 from . import test_olap_view_security
