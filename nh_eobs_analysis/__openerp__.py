82ac2a5e0a6d65c19ba2956a0cc938ba8fcfac88
Will Earp <will.earp@icloud.com>
2016-01-20 11:56:24 +0000

    [IMP][WI-1961] Added new module nh_eobs_analysis. Added NEWS analysis item to menuitem.

---
+++ nh_eobs_analysis/__openerp__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+# Part of Open eObs. See LICENSE file for full copyright and licensing details.
+# -*- encoding: utf-8 -*-
+{
+    'name': 'NH eObs Analysis',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Neova Health',
+    'website': 'http://www.neovahealth.co.uk/',
+    'depends': ['nh_eobs'],
+    'data': ['views/news.xml'],
+    'qweb': [],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
