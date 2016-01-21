ec02492f99e2e194618f8b81070ffcdf723f48bd
Will Earp <will.earp@icloud.com>
2016-01-21 11:58:12 +0000

    [IMP][WI-1961] Added tests for my dashboard.

---
+++ nh_eobs_analysis/tests/test_my_dashboard.py
@@ -0,0 +1,17 @@
+# Part of Open eObs. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+from openerp.tests.common import TransactionCase
+
+
+class TestMyDashboard(TransactionCase):
+    """
+    Test the following aspect of the 'My Dashboard' view
+    - It should allow the user to save the filtered dataset of the OLAP
+      view to the 'My Dashboard' view.
+    """
+
+    def test_OLAP_view_is_saved_to_my_dashboard(self):
+        """
+        Test that My Dashboard saves the filtered dataset of OLAP.
+        """
+        pass
