e717dc9edf6dc42cedc375d8989f623a386a9f91
Colin Wren <colin@neovahealth.co.uk>
2016-02-03 16:06:54 +0000

    Refactoring NHMobile JavaScript library to use Karma instead of deprecated gulp-karma

---
+++ nh_eobs_mobile/static/dev/coffee/travis_karma.conf.js
@@ -0,0 +1,56 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+
+        files: [
+            'tests/src/*js',
+            'tests/lib/helpers.js',
+            'tests/lib/test_routes.js',
+            'tests/spec/conversion.spec.js',
+		    'tests/spec/network.spec.js',
+		    'tests/spec/patient_info.spec.js',
+		    'tests/spec/utility.spec.js',
+            'tests/spec/standin.spec.js',
+            'tests/spec/form.spec.js',
+		    'tests/spec/events.spec.js'
+        ],
+
+        exclude: [],
+
+        hostname: 'localhost',
+        port: 9876,
+
+        reporters: ['progress'],
+
+        preprocessors: {
+            'tests/src/*.js': ['coverage']
+        },
+
+        autoWatch: false,
+        singleRun: true,
+
+        frameworks: ['jasmine-ajax', 'jasmine'],
+
+        browsers: ['PhantomJS'],
+
+        plugins: [
+            'karma-jasmine',
+            'karma-jasmine-ajax',
+            'karma-junit-reporter',
+            'karma-phantomjs-launcher',
+            'karma-coverage',
+            'karma-nyan-reporter',
+            'karma-html-reporter'
+        ],
+
+        junitReporter: {
+            outputFile: 'unit.xml',
+            suite: 'unit'
+        },
+
+        // optionally, configure the reporter
+        coverageReporter: {
+          type : 'text'
+        },
+    })
+}
