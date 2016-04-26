8d7dda9d78a694c1981f62242d39b625ed156041
Colin Wren <colin@neovahealth.co.uk>
2016-04-26 22:21:17 +0100

    Finishing Review Frequency tests

--- nh_observations/tests/test_review_frequency.py
+++ nh_observations/tests/test_review_frequency.py
@@ -12,7 +12,7 @@ class TestReviewFrequency(SingleTransactionCase):
         cls.activity_pool = cls.registry('nh.activity')
 
         def mock_ews_id(*args, **kwargs):
-            return [1]
+            return [9001]
         cls.activity_pool._patch_method('search', mock_ews_id)
 
     @classmethod
@@ -20,16 +20,29 @@ class TestReviewFrequency(SingleTransactionCase):
         cls.activity_pool._revert_method('search')
         super(TestReviewFrequency, cls).tearDownClass()
 
-    def test_15_minutes(self):
-        meh = self.activity_pool.create(self.cr, self.uid, {
+    def setUp(self):
+        super(TestReviewFrequency, self).setUp()
+        self.activity = self.activity_pool.new(self.cr, self.uid, {
             'data_model': 'nh.clinical.patient.observation'})
 
+    def tearDown(self):
+        self.activity_pool._revert_method('browse')
+        super(TestReviewFrequency, self).tearDown()
+
+    def setUpMock(self, frequency):
         def mock_activity_browse(*args, **kwargs):
-            self.activity_pool.read(self.cr, self.uid, meh)
-            meh.data_ref.frequency = 15
-            return meh
+            if len(args) > 2 and args[3] == 9001:
+                self.activity.data_ref = \
+                    self.registry('nh.clinical.patient.observation') \
+                        .new(self.cr, self.uid, {'frequency': frequency})
+                return self.activity
+            else:
+                return mock_activity_browse.origin(*args, **kwargs)
+
         self.activity_pool._patch_method('browse', mock_activity_browse)
 
+    def test_15_minutes(self):
+        self.setUpMock(15)
         form_description = self.review_frequency_pool.get_form_description(
             self.cr, self.uid, 1, context=None)
 
@@ -37,3 +50,81 @@ class TestReviewFrequency(SingleTransactionCase):
             form_description[0]['selection'],
             [(15, 'Every 15 Minutes')]
         )
+
+    def test_30_minutes(self):
+        self.setUpMock(30)
+        form_description = self.review_frequency_pool.get_form_description(
+            self.cr, self.uid, 1, context=None)
+
+        self.assertEqual(
+            form_description[0]['selection'],
+            [
+                (15, 'Every 15 Minutes'),
+                (30, 'Every 30 Minutes')
+            ]
+        )
+
+    def test_60_minutes(self):
+        self.setUpMock(60)
+        form_description = self.review_frequency_pool.get_form_description(
+            self.cr, self.uid, 1, context=None)
+
+        self.assertEqual(
+            form_description[0]['selection'],
+            [
+                (15, 'Every 15 Minutes'),
+                (30, 'Every 30 Minutes'),
+                (60, 'Every Hour')
+            ]
+        )
+
+    def test_6_hours(self):
+        self.setUpMock(360)
+        form_description = self.review_frequency_pool.get_form_description(
+            self.cr, self.uid, 1, context=None)
+
+        self.assertEqual(
+            form_description[0]['selection'],
+            [
+                (15, 'Every 15 Minutes'),
+                (30, 'Every 30 Minutes'),
+                (60, 'Every Hour'),
+                (120, 'Every 2 Hours'),
+                (240, 'Every 4 Hours'),
+                (360, 'Every 6 Hours'),
+            ]
+        )
+
+    def test_24_hours(self):
+        self.setUpMock(1440)
+        form_description = self.review_frequency_pool.get_form_description(
+            self.cr, self.uid, 1, context=None)
+
+        self.assertEqual(
+            form_description[0]['selection'],
+            [
+                (15, 'Every 15 Minutes'),
+                (30, 'Every 30 Minutes'),
+                (60, 'Every Hour'),
+                (120, 'Every 2 Hours'),
+                (240, 'Every 4 Hours'),
+                (360, 'Every 6 Hours'),
+                (480, 'Every 8 Hours'),
+                (600, 'Every 10 Hours'),
+                (720, 'Every 12 Hours'),
+                (1440, 'Every Day')
+            ]
+        )
+
+    def test_25_hours(self):
+        with self.assertRaises(ValueError):
+            self.setUpMock(1500)
+            self.review_frequency_pool.get_form_description(self.cr, self.uid,
+                                                            1, context=None)
+
+    def test_0_minutes(self):
+        self.setUpMock(0)
+        form_description = self.review_frequency_pool.get_form_description(
+            self.cr, self.uid, 1, context=None)
+
+        self.assertEqual(form_description[0]['selection'], [])
