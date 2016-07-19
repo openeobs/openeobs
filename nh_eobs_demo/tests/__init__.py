cdff99160288fd485da8fd9789262544b71a468f
Colin Wren <colin@gimpneek.com>
2016-07-19 14:20:01 +0100

    Eobs 195 patient update fix (#33)
    * Adding LDAP details to the master data for SLaM
    
    * Linting fixes for name_get implementation
    
    * Integration test to check that workload reflects patient name update
    
    * Adding integration test to patients without bed and fixing ward dashboard view

--- nh_eobs_demo/tests/__init__.py
+++ nh_eobs_demo/tests/__init__.py
@@ -15,5 +15,9 @@ from . import test_api_get_activities_settings_integration
 from . import test_wardboard_discharge_transfer_settings_integration
 from . import test_workload_bucket_settings_integration
 
+# Test Patient Name - EOBS-195
+from . import test_workload_patient_name_get
+from . import test_placement_patient_name_get
+
 # Disabled tests
 # from . import test_demo
