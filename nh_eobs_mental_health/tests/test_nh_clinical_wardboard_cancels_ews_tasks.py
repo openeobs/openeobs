49ea3ffb84bde81ec29914c00e5666e85e9b20ea
Colin Wren <colin.wren@bjss.com>
2016-09-05 17:43:28 +0100

    - Fixing failing tests

--- nh_eobs_mental_health/tests/test_nh_clinical_wardboard_cancels_ews_tasks.py
+++ nh_eobs_mental_health/tests/test_nh_clinical_wardboard_cancels_ews_tasks.py
@@ -13,6 +13,7 @@ class TestNHClinicalWardBoardCancelsEWSTasks(SingleTransactionCase):
         super(TestNHClinicalWardBoardCancelsEWSTasks, cls).setUpClass()
         cls.wardboard_model = cls.registry('nh.clinical.wardboard')
         cls.activity_model = cls.registry('nh.activity')
+        cls.spell_model = cls.registry('nh.clinical.spell')
 
         def patch_wardboard_read(*args, **kwargs):
             return {
@@ -39,6 +40,9 @@ class TestNHClinicalWardBoardCancelsEWSTasks(SingleTransactionCase):
                 return False
             return True
 
+        def patch_spell_search(*args, **kwargs):
+            return [1]
+
         def patch_cancel_open_ews(*args, **kwargs):
             context = kwargs.get('context', {})
             test = context.get('test', '')
@@ -54,6 +58,7 @@ class TestNHClinicalWardBoardCancelsEWSTasks(SingleTransactionCase):
         cls.wardboard_model._patch_method(
             'cancel_open_ews', patch_cancel_open_ews)
         cls.wardboard_model._patch_method('read', patch_wardboard_read)
+        cls.spell_model._patch_method('search', patch_spell_search)
 
     @classmethod
     def tearDownClass(cls):
@@ -62,6 +67,7 @@ class TestNHClinicalWardBoardCancelsEWSTasks(SingleTransactionCase):
         cls.wardboard_model._revert_method('toggle_obs_stop_flag')
         cls.wardboard_model._revert_method('cancel_open_ews')
         cls.wardboard_model._revert_method('read')
+        cls.spell_model._revert_method('search')
 
     def test_cancels_open_activities(self):
         self.wardboard_model.toggle_obs_stop(
