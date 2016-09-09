a3473752f9372d6f1203e8cd426b992d14d31d9a
Colin Wren <colin.wren@bjss.com>
2016-09-09 18:09:22 +0100

    - Adding a new materialized view refresh method to work with the move v8 api as the openerp.api methods don't seem to work when decorating decorator functions

--- nh_eobs/helpers.py
+++ nh_eobs/helpers.py
@@ -25,3 +25,25 @@ def refresh_materialized_views(*views):
             return result
         return _complete
     return _refresh_materialized_views
+
+
+def v8_refresh_materialized_views(*views):
+    """
+    Decorator method to refresh materialized views passed
+    as arguments.
+    :param views: name(s) of materialized view(s) to refresh
+    :return: True if activity is completed
+    """
+    def _refresh_materialized_views(f):
+        @wraps(f)
+        def _complete(*args, **kwargs):
+            self = args[0]
+            result = f(*args, **kwargs)
+            sql = ''
+            for view in views:
+                sql += 'refresh materialized view ' + view + ';\n'
+            self._cr.execute(sql)
+            _logger.debug('Materialized view(s) refreshed')
+            return result
+        return _complete
+    return _refresh_materialized_views
--- nh_eobs_mental_health/models/nh_clinical_wardboard.py
+++ nh_eobs_mental_health/models/nh_clinical_wardboard.py
@@ -2,6 +2,7 @@ from openerp.osv import orm, osv, fields
 from datetime import datetime, timedelta
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 from openerp import api
+from openerp.addons.nh_eobs import helpers
 
 
 class NHClinicalWardboard(orm.Model):
@@ -29,7 +30,6 @@ class NHClinicalWardboard(orm.Model):
         'obs_stop': fields.function(_get_obs_stop_from_spell, type='boolean')
     }
 
-    # @helpers.refresh_materialized_views('ews0', 'ews1', 'ews2')
     @api.multi
     def toggle_obs_stop(self):
         """
@@ -92,6 +92,7 @@ class NHClinicalWardboard(orm.Model):
             'view_id': view_id
         }
 
+    @helpers.v8_refresh_materialized_views('ews0', 'ews1', 'ews2')
     @api.multi
     def start_patient_monitoring_exception(self, reasons, spell_id,
                                            spell_activity_id):
@@ -132,6 +133,7 @@ class NHClinicalWardboard(orm.Model):
 
         self.set_obs_stop_flag(spell_id, True)
 
+    @helpers.v8_refresh_materialized_views('ews0', 'ews1', 'ews2')
     @api.multi
     def end_patient_monitoring_exception(self):
         """
