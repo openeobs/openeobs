6fc3d4d70de9ea72eed0dbffd863a1a554bace90
Daniel Metcalfe <daniel.metcalfe@nhtek.io>
2016-09-22 12:32:16 +0100

    Fixed flake8 error.

--- nh_eobs/tests/observation_report/test_get_patient_monitoring_exception_report_data.py
+++ nh_eobs/tests/observation_report/test_get_patient_monitoring_exception_report_data.py
@@ -178,8 +178,8 @@ class TestGetPatientMonitoringExceptionReportData(TransactionCase):
 
         self.dictionary = self.observation_report_model\
             .get_patient_monitoring_exception_report_data(
-            self.spell_activity.id, self.start_date, self.end_date
-        )
+                self.spell_activity.id, self.start_date, self.end_date
+            )
 
         self.report_entries = self.dictionary[self.root_key]
 
