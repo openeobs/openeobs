08b1fff1b61a0483928b7f0077616f72acac7542
Colin Wren <colin.wren@bjss.com>
2016-11-23 15:45:47 +0000

    - Restoring tests

--- nh_eobs_mental_health/tests/__init__.py
+++ nh_eobs_mental_health/tests/__init__.py
@@ -1,10 +1,10 @@
 from .nh_eobs_mobile_mental import *
-# from . import test_spell_obs_stop_property
-# from .nh_clinical_doctor_activities import *
-# from .nh_clinical_overdue import *
-# from .nh_clinical_patient_monitoring_exception import *
-# from .nh_clinical_patient_observation_ews import *
-# from .nh_clinical_wardboard import *
-# from .nh_eobs_api import *
-# from .nh_eobs_settings import *
-# from .nh_eobs_ward_dashboard import *
+from . import test_spell_obs_stop_property
+from .nh_clinical_doctor_activities import *
+from .nh_clinical_overdue import *
+from .nh_clinical_patient_monitoring_exception import *
+from .nh_clinical_patient_observation_ews import *
+from .nh_clinical_wardboard import *
+from .nh_eobs_api import *
+from .nh_eobs_settings import *
+from .nh_eobs_ward_dashboard import *
