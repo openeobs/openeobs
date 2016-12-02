75581163a7fda04669a4245a62e0d08df5d25868
Colin Wren <colin.wren@bjss.com>
2016-12-02 11:51:25 +0000

    - Fixing tests ( I hope )

--- nh_eobs_mental_health/tests/nh_eobs_api/test_get_activities_refused.py
+++ nh_eobs_mental_health/tests/nh_eobs_api/test_get_activities_refused.py
@@ -54,8 +54,7 @@ class TestGetActivitiesRefused(TransactionObservationCase):
         refusal_in_effect is set to True
         """
         self.complete_obs(clinical_risk_sample_data.REFUSED_DATA)
-        activities = self.api_pool.get_activities(
-            self.cr, self.user_id, [self.patient_id])
+        activities = self.api_pool.get_activities(self.cr, self.user_id, [])
         self.assertTrue(activities[1].get('refusal_in_effect'))
 
     def test_refused_no_risk(self):
