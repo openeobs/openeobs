53fc83bd3b90c6d91e552b2b2ffb165ca5a6c723
Colin Wren <colin.wren@bjss.com>
2017-01-06 14:30:41 +0000

    - Fixing import issue

--- nh_eobs_mental_health/__init__.py
+++ nh_eobs_mental_health/__init__.py
@@ -1,7 +1,6 @@
 from .models import nh_clinical_doctor_activities
 from .models import nh_clinical_notification_clinical_review
 from .models import nh_clinical_notification_clinical_review_frequency
-from .models import nh_clinical_observation_report
 from .models import nh_clinical_overdue
 from .models import nh_clinical_patient_observation_ews
 from .models import nh_clinical_settings
@@ -11,6 +10,7 @@ from .models import nh_clinical_wardboard
 from .models import nh_eobs_api
 from .models import nh_eobs_mobile_main
 from .models import nh_eobs_ward_dashboard
+from .models import nh_clinical_observation_report
 from .models import report_nh_clinical_observation_report
 from .tests.common import nh_clinical_test_utils
 from .wizard import nh_clinical_patient_monitoring_exception_select_reason
