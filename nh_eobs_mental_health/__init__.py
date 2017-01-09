489e6f723eb29a8270eee235c98effe557c9c671
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-01-09 16:14:09 +0000

    Deleted unused file.

--- nh_eobs_mental_health/__init__.py
+++ nh_eobs_mental_health/__init__.py
@@ -1,3 +1,5 @@
+from openeobs.nh_eobs_mental_health.report import \
+    clinical_review, refused_observation
 from .models import nh_clinical_doctor_activities
 from .models import nh_clinical_notification_clinical_review
 from .models import nh_clinical_notification_clinical_review_frequency
@@ -5,12 +7,10 @@ from .models import nh_clinical_overdue
 from .models import nh_clinical_patient_observation_ews
 from .models import nh_clinical_settings
 from .models import nh_clinical_spell
-from .sql import *
 from .models import nh_clinical_wardboard
 from .models import nh_eobs_api
 from .models import nh_eobs_mobile_main
 from .models import nh_eobs_ward_dashboard
-from .models import nh_clinical_observation_report
-from .models import report_nh_clinical_observation_report
+from .sql import *
 from .tests.common import nh_clinical_test_utils
 from .wizard import nh_clinical_patient_monitoring_exception_select_reason
