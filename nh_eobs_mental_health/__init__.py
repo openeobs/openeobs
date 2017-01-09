cbab599f5da7e1331892a848a958c95fb47d914e
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-01-09 17:10:07 +0000

    Changed import.

--- nh_eobs_mental_health/__init__.py
+++ nh_eobs_mental_health/__init__.py
@@ -1,5 +1,3 @@
-from openeobs.nh_eobs_mental_health.report import \
-    clinical_review, refused_observation
 from .models import nh_clinical_doctor_activities
 from .models import nh_clinical_notification_clinical_review
 from .models import nh_clinical_notification_clinical_review_frequency
@@ -11,6 +9,8 @@ from .models import nh_clinical_wardboard
 from .models import nh_eobs_api
 from .models import nh_eobs_mobile_main
 from .models import nh_eobs_ward_dashboard
+from .report import clinical_review, refused_observation
 from .sql import *
 from .tests.common import nh_clinical_test_utils
 from .wizard import nh_clinical_patient_monitoring_exception_select_reason
+
