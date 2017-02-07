b0f7a1cc3a8a66a18cad4b0c8a7190cb0531590e
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-02-07 22:31:35 +0000

    Refactored neurological model to remove duplication.
    Created model for creating form description dictionaries describing fields from the field objects on models.

--- neurological_observations/models/nh_clinical_patient_observation_neurological.py
+++ neurological_observations/models/nh_clinical_patient_observation_neurological.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields
 
+
 class NhClinicalPatientObservationNeurological(models.Model):
+
+    def init(self, cr):
+        self.set_form_description()
 
     _name = 'nh.clinical.patient.observation.neurological'
     _inherit = 'nh.clinical.patient.observation.gcs'
@@ -16,163 +20,51 @@ class NhClinicalPatientObservationNeurological(models.Model):
         ['2', '2mm'],
         ['1', '1mm'],
     ]
+    _limb_movement_selection = [
+        ('normal power', 'Normal Power'),
+        ('mild weakness', 'Mild Weakness'),
+        ('severe weakness', 'Severe Weakness'),
+        ('spastic flexion', 'Spastic Flexion'),
+        ('extension', 'Extension'), ('no response', 'No Response'),
+        ('not observable', 'Not Observable')
+    ]
+    _pupil_reaction_selection = [
+        ('yes', 'Yes'), ('no', 'No'),
+        ('sluggish', 'Sluggish')
+    ]
 
     pupil_right_size = fields.Selection(_pupil_sizes, 'Right Pupil Size')
     pupil_left_size = fields.Selection(_pupil_sizes, 'Left Pupil Size')
     pupil_right_reaction = fields.Selection(
-        [('yes', 'Yes'), ('no', 'No'), ('sluggish', 'Sluggish')],
-        'Right Pupil Reaction'
+        _pupil_reaction_selection, 'Right Pupil Reaction'
     )
     pupil_left_reaction = fields.Selection(
-        [('yes', 'Yes'), ('no', 'No'), ('sluggish', 'Sluggish')],
-        'Left Pupil Reaction'
+        _pupil_reaction_selection, 'Left Pupil Reaction'
     )
     limb_movement_left_arm = fields.Selection(
-        [
-            ('normal power', 'Normal Power'),
-            ('mild weakness', 'Mild Weakness'),
-            ('severe weakness', 'Severe Weakness'),
-            ('spastic flexion', 'Spastic Flexion'),
-            ('extension', 'Extension'),
-            ('no response', 'No Response'),
-            ('not observable', 'Not Observable')
-        ],
-        'Limb Movement - Left Arm'
+        _limb_movement_selection, 'Limb Movement - Left Arm'
     )
     limb_movement_right_arm = fields.Selection(
-        [
-            ('normal power', 'Normal Power'),
-            ('mild weakness', 'Mild Weakness'),
-            ('severe weakness', 'Severe Weakness'),
-            ('spastic flexion', 'Spastic Flexion'),
-            ('extension', 'Extension'),
-            ('no response', 'No Response'),
-            ('not observable', 'Not Observable')
-        ],
-        'Limb Movement - Right Arm'
+        _limb_movement_selection, 'Limb Movement - Right Arm'
     )
     limb_movement_left_leg = fields.Selection(
-        [
-            ('normal power', 'Normal Power'),
-            ('mild weakness', 'Mild Weakness'),
-            ('severe weakness', 'Severe Weakness'),
-            ('spastic flexion', 'Spastic Flexion'),
-            ('extension', 'Extension'),
-            ('no response', 'No Response'),
-            ('not observable', 'Not Observable')
-        ],
-        'Limb Movement - Left Leg'
+        _limb_movement_selection, 'Limb Movement - Left Leg'
     )
     limb_movement_right_leg = fields.Selection(
-        [
-            ('normal power', 'Normal Power'),
-            ('mild weakness', 'Mild Weakness'),
-            ('severe weakness', 'Severe Weakness'),
-            ('spastic flexion', 'Spastic Flexion'),
-            ('extension', 'Extension'),
-            ('no response', 'No Response'),
-            ('not observable', 'Not Observable')
-        ],
-        'Limb Movement - Right leg'
+        _limb_movement_selection, 'Limb Movement - Right leg'
     )
 
-    def get_form_description(self, cr, uid, patient_id, context=None):
-        form_description = list(self._form_description)
-        form_description.append({
-            'name': 'pupil_right_size',
-            'type': 'selection',
-            'label': 'Pupil Right - Size',
-            'selection': self._pupil_sizes,
-            'selection_type': 'text',
-            'initially_hidden': False
-        })
-        form_description.append({
-            'name': 'pupil_right_reaction',
-            'type': 'selection',
-            'label': 'Pupil Right - Reaction',
-            'selection': [['yes', 'Yes'], ['no', 'No'],
-                          ['sluggish', 'Sluggish']],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        form_description.append({
-            'name': 'pupil_left_size',
-            'type': 'selection',
-            'label': 'Pupil Left - Size',
-            'selection': self._pupil_sizes,
-            'selection_type': 'text',
-            'initially_hidden': False
-        })
-        form_description.append({
-            'name': 'pupil_left_reaction',
-            'type': 'selection',
-            'label': 'Pupil Left - Reaction',
-            'selection': [['yes', 'Yes'], ['no', 'No'], ['sluggish', 'Sluggish']],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        form_description.append({
-            'name': 'limb_movement_left_arm',
-            'type': 'selection',
-            'label': 'Limb Movement - Left Arm',
-            'selection': [
-                ('normal power', 'Normal Power'),
-                ('mild weakness', 'Mild Weakness'),
-                ('severe weakness', 'Severe Weakness'),
-                ('spastic flexion', 'Spastic Flexion'),
-                ('extension', 'Extension'),
-                ('no response', 'No Response'),
-                ('not observable', 'Not Observable')
-            ],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        form_description.append({
-            'name': 'limb_movement_right_arm',
-            'type': 'selection',
-            'label': 'Limb Movement - Right Arm',
-            'selection': [
-                ('normal power', 'Normal Power'),
-                ('mild weakness', 'Mild Weakness'),
-                ('severe weakness', 'Severe Weakness'),
-                ('spastic flexion', 'Spastic Flexion'),
-                ('extension', 'Extension'),
-                ('no response', 'No Response'),
-                ('not observable', 'Not Observable')
-            ],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        form_description.append({
-            'name': 'limb_movement_left_leg',
-            'type': 'selection',
-            'label': 'Limb Movement - Left Leg',
-            'selection': [
-                ('normal power', 'Normal Power'),
-                ('mild weakness', 'Mild Weakness'),
-                ('severe weakness', 'Severe Weakness'),
-                ('spastic flexion', 'Spastic Flexion'),
-                ('extension', 'Extension'),
-                ('no response', 'No Response'),
-                ('not observable', 'Not Observable')
-            ],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        form_description.append({
-            'name': 'limb_movement_right_leg',
-            'type': 'selection',
-            'label': 'Limb Movement - Right Leg',
-            'selection': [
-                ('normal power', 'Normal Power'),
-                ('mild weakness', 'Mild Weakness'),
-                ('severe weakness', 'Severe Weakness'),
-                ('spastic flexion', 'Spastic Flexion'),
-                ('extension', 'Extension'),
-                ('no response', 'No Response'),
-                ('not observable', 'Not Observable')
-            ],
-            'selection_type': 'text',
-            'initially_hidden': False,
-        })
-        return form_description
+    def set_form_description(self):
+        # env not available when called from init but pool is.
+        converter = self.pool['field_to_form_description_converter']
+        form_description_neuro_fields = converter.convert([
+            self._fields['pupil_right_size'],
+            self._fields['pupil_left_size'],
+            self._fields['pupil_right_reaction'],
+            self._fields['pupil_left_reaction'],
+            self._fields['limb_movement_left_arm'],
+            self._fields['limb_movement_right_arm'],
+            self._fields['limb_movement_left_leg'],
+            self._fields['limb_movement_right_leg']
+        ])
+        self._form_description.extend(form_description_neuro_fields)
