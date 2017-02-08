2c77aac04ae091b4b1a7be487f8110d406bb3767
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-02-08 21:51:58 +0000

    Added another test class skeleton.

---
+++ neurological_observations/tests/nh_clinical_patient_observation_neurological/test_pupil_reference_chart.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import SingleTransactionCase
+
+
+class TestPupilReferenceChart(SingleTransactionCase):
+
+    def test_returns_200(self):
+        pass
+
+    def test_returns_html(self):
+        pass
+
