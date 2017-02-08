e349c861304833072c2109fbbcfe31985ded09a0
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-02-08 21:49:40 +0000

    Added test class skeletons.

---
+++ neurological_observations/tests/nh_clinical_patient_observation_neurological/test_get_form_description.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import SingleTransactionCase
+
+
+class TestGetFormDescription(SingleTransactionCase):
+
+    def test_has_fields_metadata(self):
+        pass
+
+    def test_mandatory_keys_on_fields(self):
+        pass
+
+    def test_selections(self):
+        pass
+
+    def test_labels(self):
+        pass
---
+++ neurological_observations/tests/nh_clinical_patient_observation_neurological/test_is_partial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from openerp.tests.common import SingleTransactionCase
+
+
+class TestIsPartial(SingleTransactionCase):
+
+    def test_full_with_mandatory_fields_populated(self):
+        pass
+
+    def test_partial_with_no_mandatory_fields_populated(self):
+        pass
+
+    def test_partial_with_one_mandatory_field_populated(self):
+        pass
+
+    def test_partial_with_all_optional_fields_populated(self):
+        pass
+
+    def test_partial_with_no_fields_populated(self):
+        pass
