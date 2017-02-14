fcbde56e81d05d0cdad2aee7fb537d122a097433
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-02-14 10:20:04 +0000

    Added pupil reference chart tests.

--- neurological_observations/tests/controllers/test_pupil_reference_chart.py
+++ neurological_observations/tests/controllers/test_pupil_reference_chart.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-from openerp.tests.common import SingleTransactionCase
+from openerp.tests.common import HttpCase
 
 
-class TestPupilReferenceChart(SingleTransactionCase):
+class TestPupilReferenceChart(HttpCase):
+
+    def setUp(self):
+        super(TestPupilReferenceChart, self).setUp()
+        self.authenticate('nasir', 'nasir')
+        self.url = '/mobile/src/html/pupil_size_chart.html'
 
     def test_returns_200(self):
-        pass
+        response = self.url_open(self.url)
+        self.assertEqual(200, response.code)
 
     def test_returns_html(self):
-        pass
+        response = self.url_open(self.url)
+        self.assertEqual('text/html', response.header.type)
