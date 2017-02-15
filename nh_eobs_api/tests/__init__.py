6f155662a596822b8cec31e13af450f0f85ba7f2
Colin Wren <colin.wren@bjss.com>
2017-02-15 00:16:27 +0000

    - Removing function after finding out the method I thought was causing the bug was always returning a set value due to the key it was looking for being removed from the data dict before calling the method

--- nh_eobs_api/tests/__init__.py
+++ nh_eobs_api/tests/__init__.py
@@ -1,6 +1,5 @@
 # Part of Open eObs. See LICENSE file for full copyright and licensing details.
 import os
-from . import test_get_next_action
 if not os.environ.get('TRAVIS'):
     from . import test_routing
     from . import test_controller_api
