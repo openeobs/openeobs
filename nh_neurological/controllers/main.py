30ad1b7961ee34e41ffc0b96da84cde293e05d97
Colin Wren <colin.wren@bjss.com>
2017-02-20 19:52:46 +0000

    - Fixing EOBS-1015 by removing `@staticmethod` decorator as was preventing route from working

--- nh_neurological/controllers/main.py
+++ nh_neurological/controllers/main.py
@@ -8,10 +8,17 @@ URL_PREFIX = '/mobile/'
 
 class NeuroMobileFrontend(MobileFrontend):
 
-    @staticmethod
     @http.route(
         URL_PREFIX + 'src/html/pupil_size_chart.html',
         type="http", auth="none")
-    def pupil_size_chart():
+    def pupil_size_chart(self, *args, **kwargs):
+        """
+        URL to render the pupil size chart HTML so it can be displayed in an
+        iframe for the Pupil Size Reference Chart
+
+        :param args: URL arguments
+        :param kwargs: query string arguments for route
+        :return: HTTP Response
+        """
         return request.render('nh_neurological.pupil_size_reference',
                               qcontext={})
