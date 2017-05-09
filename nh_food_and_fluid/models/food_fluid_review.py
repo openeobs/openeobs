7b5448a44b270db2e56974f7aedcd34009c1a19f
Colin Wren <colin@gimpneek.com>
2017-05-09 13:57:10 +0100

    Eobs 1221 trigger review task (#107)
    * - Fixing 283: Report period now shows context away (i.e. shows BST) dates
    
    * - Adding method to give a boolean view on if there has been observations in the current period
    
    * - Adding method to be called by cron job
    
    * - Removing localised_trigger_time.py stuff cos we can rely on Odoo's ir.cron to use the timezone for the user so by setting the Europe/London timezone to the Admin user we can do this
    
    * - Adding test to ensure that only HCA, Nurse and Doctors associated with patient location can see the task in their lists
    
    * - I hate timezones / Fixing Travis build by ensuring localised timezone call is mocked out
    
    * - Reverting back to checking localised time every hour cos it turns out that ir.cron doesn't do this well
    
    * - Removing forcecreate as is the default
    
    * - Refactoring as per Dan's instructions
    
    * - Making sure record in `nh_clinical_notification_food_fluid_review` table is being generated as it only gets created when you add an entry into second dictionary of `create_activity` for some reason
    
    * - Unchecking repeat missed as this results in it running the cron job X times (for each missed hour) and that will then (assuming it runs at the 0600/1500) trigger X many tasks

---
+++ nh_food_and_fluid/models/food_fluid_review.py
@@ -0,0 +1,85 @@
+from openerp import models, api
+from datetime import datetime
+
+
+class FoodAndFluidReview(models.Model):
+    """
+    Food and Fluid Review task
+    """
+
+    _name = 'nh.clinical.notification.food_fluid_review'
+    _inherit = 'nh.clinical.notification'
+
+    _description = 'F&F - {} Fluid Intake Review'
+
+    trigger_times = [15, 6]
+
+    @api.model
+    def should_trigger_review(self):
+        """
+        Take the current localised time for the user and figure out if the
+        review task should be triggered
+        :return: True if correct localised time
+        :rtype: bool
+        """
+        dateutils_model = self.env['datetime_utils']
+        localised_time = dateutils_model.get_localised_time()
+        if localised_time.hour in self.trigger_times:
+            return True
+        return False
+
+    @api.model
+    def get_review_task_summary(self):
+        """
+        Get the summary for the review task
+        :return: string for summary
+        :rtype: str
+        """
+        dateutils_model = self.env['datetime_utils']
+        localised_time = dateutils_model.get_localised_time()
+        return self._description.format(
+            datetime.strftime(localised_time, '%-I%p').lower())
+
+    @api.model
+    def trigger_review_tasks_for_active_periods(self):
+        """
+        Method to trigger F&F review tasks for any active periods in the system
+        Called by Scheduled Action every hour
+        """
+        food_fluid_model = \
+            self.env['nh.clinical.patient.observation.food_fluid']
+        is_time_to_trigger_review = self.should_trigger_review()
+        if is_time_to_trigger_review:
+            activity_model = self.env['nh.activity']
+            spell_activities = activity_model.search(
+                [
+                    ['data_model', '=', 'nh.clinical.spell'],
+                    ['state', 'not in', ['completed', 'cancelled']]
+                ]
+            )
+            for spell_activity in spell_activities:
+                if food_fluid_model.active_food_fluid_period(
+                        spell_activity.id):
+                    self.schedule_review(spell_activity)
+
+    def schedule_review(self, spell_activity):
+        """
+        Create the activity for the Food and Fluid Review Task
+        :param spell_activity: Activity for patient's spell
+        :return: activity ID
+        """
+        dateutils_model = self.env['datetime_utils']
+        return self.create_activity(
+            {
+                'parent_id': spell_activity.id,
+                'spell_activity_id': spell_activity.id,
+                'patient_id': spell_activity.patient_id.id,
+                'summary': self.get_review_task_summary(),
+                'location_id': spell_activity.location_id.id,
+                'date_scheduled': dateutils_model.get_current_time(
+                    as_string=True)
+            },
+            {
+                'patient_id': spell_activity.patient_id.id
+            }
+        )
