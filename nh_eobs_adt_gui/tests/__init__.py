76870cf6539f950e704cab7cc4ffe62ae4e07fb1
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-07-20 16:22:25 +0100

    Moved tests into subpackage.

--- nh_eobs_adt_gui/tests/__init__.py
+++ nh_eobs_adt_gui/tests/__init__.py
@@ -1,4 +1,3 @@
-from . import test_spell_management
 from . import test_wizards
-from . import test_spell_management_patient_update
+from . import test_wizards
 from .nh_clinical_spellboard import *
--- nh_eobs_adt_gui/tests/nh_clinical_spellboard/__init__.py
+++ nh_eobs_adt_gui/tests/nh_clinical_spellboard/__init__.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
 from . import test_write
+from . import test_spell_management
+from . import test_spell_management_patient_update
