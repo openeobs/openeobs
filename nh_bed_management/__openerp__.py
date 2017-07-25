1a6c3f2f8e0c53055155f7fb3671eea9271493a1
Colin Wren <colin.wren@bjss.com>
2017-07-25 12:22:54 +0100

    - Adding bed management module skeleton

---
+++ nh_bed_management/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# Part of Open eObs. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'LiveObs Bed Management',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': 'Bed Management for LiveObs',
+    'description': """Bed Management for LiveObs""",
+    'author': 'BJSS',
+    'website': 'http://bjss.com',
+    'depends': ['nh_clinical'],
+    'data': [],
+    'qweb': [],
+    'application': True,
+    'installable': True,
+    'active': False,
+    }
