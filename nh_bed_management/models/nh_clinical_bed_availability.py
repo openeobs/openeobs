c750fd5308e63ab17af04598febde03bd6801f40
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-07-26 13:49:18 +0100

    Can view records but ordering does not work.
    When clicking on the column names in the list view, another `search_read` call is made passing the appropriate argument for the `order` parameter. I have overridden the method but do not handle this argument so it currently has no effect.

--- nh_bed_management/models/nh_clinical_bed_availability.py
+++ nh_bed_management/models/nh_clinical_bed_availability.py
@@ -1,11 +1,80 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 
 
-class NhClinicalBedAvailability(models.Model):
+class NhClinicalBedAvailability(models.AbstractModel):
 
     _name = 'nh.clinical.bed_availability'
+    _order = 'bed_status desc'
 
-    locations = fields.Many2one(
-        'nh.clinical.location', 'LOCATIONS', required=True
-    )
+    hospital_name = fields.Char()
+    ward_name = fields.Char()
+    bed_name = fields.Char()
+    bed_status = fields.Char()
+    patient_status = fields.Char()
+
+    @api.multi
+    def search_read(self, domain=None, fields=None, offset=0, limit=None,
+                    order=None):
+        location_model = self.env['nh.clinical.location']
+        # Read all hospitals, wards, and beds.
+        locations = location_model.search_read(
+            domain=[('usage', 'in', ['hospital', 'ward', 'bed'])],
+            fields=['parent_id', 'name', 'patient_ids', 'usage'],
+            offset=offset,
+            limit=limit
+        )
+
+        bed_availability_records = []
+        for location in locations:
+            hospital_name, ward_name, bed_name = self._get_location_names(
+                location)
+            bed_status = self._get_bed_status(location)
+            patient_status = 'Unknown'
+
+            bed_availability_record = {
+                'id': location['id'],
+                'hospital_name': hospital_name,
+                'ward_name': ward_name,
+                'bed_name': bed_name,
+                'bed_status': bed_status,
+                'patient_status': patient_status,
+            }
+            bed_availability_records.append(bed_availability_record)
+
+        return bed_availability_records
+
+    def _get_location_names(self, location):
+        location_names = [None] * 3
+        usage = location['usage']
+
+        if isinstance(location['parent_id'], tuple):
+            # Is actually a 'reference' tuple, not an ID.
+            parent_id = location['parent_id'][0]
+
+        if usage == 'hospital':
+            location_names[0] = location['name']
+
+        elif usage == 'ward':
+            location_model = self.env['nh.clinical.location']
+            location_names[0] = location_model.browse(parent_id).name
+            location_names[1] = location['name']
+
+        elif usage == 'bed':
+            location_model = self.env['nh.clinical.location']
+            ward = location_model.browse(parent_id)
+            hospital = ward.parent_id
+
+            location_names[0] = hospital.name
+            location_names[1] = ward.name
+            location_names[2] = location['name']
+
+        return location_names
+
+    @staticmethod
+    def _get_bed_status(location):
+        if location['usage'] == 'bed':
+            if len(location['patient_ids']):
+                return 'Occupied'
+            else:
+                return 'Available'
