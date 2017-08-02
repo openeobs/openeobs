664f122e36a8b8adbd9433e5a6705c02938964b7
Colin Wren <colin.wren@bjss.com>
2017-08-02 17:16:51 +0100

    - Using SauceLabs branch to run tests instead of running these on Travis

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 travis_run_tests &
-git clone https://github.com/BJSS/BJSS_liveobs_automation.git
+git clone https://github.com/BJSS/BJSS_liveobs_automation.git@SauceLabs-Browser
 cd BJSS_liveobs_automation
 pip install -r requirements.txt
 behave features
