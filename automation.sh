d1645ba12f69d8efe00a6cb38ea6d77508be6932
Colin Wren <colin.wren@bjss.com>
2017-08-02 17:28:30 +0100

    - Manually branch

--- automation.sh
+++ automation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 travis_run_tests &
-git clone -b SauceLabs-Browser https://github.com/BJSS/BJSS_liveobs_automation.git
+git clone https://github.com/BJSS/BJSS_liveobs_automation.git
 cd BJSS_liveobs_automation
+git fetch
+git checkout SauceLabs-Browser
 pip install -r requirements.txt
 behave features
