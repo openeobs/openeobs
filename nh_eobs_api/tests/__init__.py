95944fc19bd27919b7238d672061356eb5d8312f
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-09-19 16:44:58 +0100

    See if HTTP test works in CI now.
    HTTP test cases fail in Travis. Suspect that this is caused by the `xml_test_output` plugin that is used in Travis.
    We now have GoCD in place which does not use `xml_test_output` so seeing if HTTP test cases will work now.

--- nh_eobs_api/tests/__init__.py
+++ nh_eobs_api/tests/__init__.py
@@ -1,5 +1,7 @@
 # Part of Open eObs. See LICENSE file for full copyright and licensing details.
 import os
+
+from . import test_controller_api
+
 if not os.environ.get('TRAVIS'):
     from . import test_routing
-    from . import test_controller_api
