c3abb271465b80937b0a1886a001a2e5810cd536
Colin Wren <colin.wren@bjss.com>
2017-12-04 16:14:47 +0000

    - Enabling test_print_report

--- nh_eobs_backup/tests/nh_eobs_api/__init__.py
+++ nh_eobs_backup/tests/nh_eobs_api/__init__.py
@@ -3,4 +3,4 @@ from . import test_add_report_to_backup_location
 from . import test_get_spells
 # from . import test_get_filename
 # from . import test_get_report
-# from . import test_print_report
+from . import test_print_report
