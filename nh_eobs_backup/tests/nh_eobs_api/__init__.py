b9653ec3c875b0ac253612a89d14e1afe1e77457
Colin Wren <colin.wren@bjss.com>
2017-12-04 13:48:00 +0000

    - Enabling backup_location

--- nh_eobs_backup/tests/nh_eobs_api/__init__.py
+++ nh_eobs_backup/tests/nh_eobs_api/__init__.py
@@ -1,5 +1,5 @@
 from . import test_add_report_to_database
-# from . import test_add_report_to_backup_location
+from . import test_add_report_to_backup_location
 # from . import test_get_spells
 # from . import test_get_filename
 # from . import test_get_report
