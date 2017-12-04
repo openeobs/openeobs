7b892002c0ad2afc8ea0777707e145af0cfd76eb
Colin Wren <colin.wren@bjss.com>
2017-12-04 09:08:58 +0000

    - Seeing if issue localised to nh_eobs_api tests

--- nh_eobs_backup/tests/__init__.py
+++ nh_eobs_backup/tests/__init__.py
@@ -2,4 +2,4 @@ from .nh_clinical_location import *
 from .nh_clinical_spell import *
 from .nh_clinical_patient_observation_ews import *
 from .base_config_settings import *
-from .nh_eobs_api import *
+# from .nh_eobs_api import *
