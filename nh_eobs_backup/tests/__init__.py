9d7b5b3d854a5c4ddc3fe9b192e6dec8295c54a7
Colin Wren <colin.wren@bjss.com>
2017-12-04 12:39:53 +0000

    - trying EWS

--- nh_eobs_backup/tests/__init__.py
+++ nh_eobs_backup/tests/__init__.py
@@ -1,5 +1,5 @@
 # from .nh_clinical_location import *
-from .nh_clinical_spell import *
-# from .nh_clinical_patient_observation_ews import *
+# from .nh_clinical_spell import *
+from .nh_clinical_patient_observation_ews import *
 # from .base_config_settings import *
 # from .nh_eobs_api import *
