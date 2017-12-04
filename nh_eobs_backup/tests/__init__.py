a649df5de81e9114142abacce2891dd76f46bf7f
Colin Wren <colin.wren@bjss.com>
2017-12-04 12:04:30 +0000

    - Enabling location

--- nh_eobs_backup/tests/__init__.py
+++ nh_eobs_backup/tests/__init__.py
@@ -1,4 +1,4 @@
-# from .nh_clinical_location import *
+from .nh_clinical_location import *
 # from .nh_clinical_spell import *
 # from .nh_clinical_patient_observation_ews import *
 # from .base_config_settings import *
