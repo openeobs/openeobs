738a99cae0f2a0d85c353eed6b1d6ff5048ff280
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-02-12 13:45:56 +0000

    Added regression test for EOBS-2342.

--- nh_eobs_analysis/tests/__init__.py
+++ nh_eobs_analysis/tests/__init__.py
@@ -1,6 +1,7 @@
-# Part of Open eObs. See LICENSE file for full copyright and licensing details.
 # -*- coding: utf-8 -*-
+# Part of Open eObs. See LICENSE file for full copyright and licensing details.
 from . import test_olap_dataset_and_manipulation
 from . import test_olap_view_dimensions
 from . import test_olap_view_security
+from . import test_read_group
 from .ir_ui_menu import *
