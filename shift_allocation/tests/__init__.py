563e3b11ae07969da9bd850da2943727fb2627ab
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-03-20 13:41:02 +0000

    Uncommented test.

--- shift_allocation/tests/__init__.py
+++ shift_allocation/tests/__init__.py
@@ -3,6 +3,7 @@ from .nh_clinical_staff_reallocation import *
 from .nh_clinical_doctor_allocation import *
 from .res_user import *
 from . import test_operations
+from . import test_responsibility_allocation_wizard
 
 # Disabled Tests
 # from . import test_user_allocation
@@ -10,5 +11,4 @@ from . import test_operations
 # from . import test_allocation_responsibility_allocation
 # from . import test_allocation_unfollow
 # from . import test_get_allocation_locations
-# from . import test_responsibility_allocation_wizard
 # from . import test_user_responsibility_allocation
