1c61d5f0292a69d34b1820d145f05c885e9e0f1a
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-03-26 12:18:45 +0100

    Make UserVoice module public for Apperta.

---
+++ uservoice/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=manifest-required-author, manifest-deprecated-key
+{
+    'name': 'Uservoice Integration',
+    'version': '1.0',
+    'category': 'Feedback',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """
+    Add Uservoice Integration to Odoo
+    """,
+    'author': 'BJSS',
+    'website': 'http://www.bjss.com/',
+    'depends': [],
+    'data': [
+        'views/add-widget.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'css': [],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
---
+++ uservoice/static/src/js/uservoice-widget.js
@@ -0,0 +1,21 @@
+UserVoice=window.UserVoice||[];
+(function(){
+  var uv=document.createElement('script');
+  uv.type='text/javascript';
+  uv.async=true;
+  uv.src='//widget.uservoice.com/xiXrGR5j7JSb6wqDtOQJw.js';
+  var s=document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(uv,s)
+})();
+UserVoice.push(['addTrigger', {
+    mode: 'feedback'
+}]);
+UserVoice.push(['set', {
+    screenshot_enabled: false,
+    permalinks_enabled: false,
+    smartvote_enabled: false,
+    contact_enabled: false,
+    post_suggestion_enabled: true,
+    menu_enabled: false,
+    forum_id: 601582
+}]);
