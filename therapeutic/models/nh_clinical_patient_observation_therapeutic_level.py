1911946fcfe5ce3280802e6298bdb04834002b97
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-04-13 09:22:26 +0100

    Added frequency field and validation.

--- therapeutic/models/nh_clinical_patient_observation_therapeutic_level.py
+++ therapeutic/models/nh_clinical_patient_observation_therapeutic_level.py
@@ -1,5 +1,10 @@
 from openerp import models, fields, api
 
+from openerp.addons.nh_observations import frequencies
+from openerp.addons.nh_odoo_fixes import validate
+
+from openerp.exceptions import ValidationError
+
 
 class NhClinicalPatientObservationTherapeuticLevel(models.Model):
 
@@ -8,7 +13,7 @@ class NhClinicalPatientObservationTherapeuticLevel(models.Model):
     patient = fields.Many2one(comodel_name='nh.clinical.patient',
                               required=True)
     level = fields.Integer(required=True, default=1)
-    # frequency = fields.Selection()
+    frequency = fields.Selection(selection=frequencies.as_list())
 
     def default_get(self, cr, uid, fields, context=None):
         """
@@ -33,4 +38,34 @@ class NhClinicalPatientObservationTherapeuticLevel(models.Model):
         return field_defaults_dict
 
     def save(self, cr, uid, ids, context=None):
+        """
+        There is a 'Save' button in the 'Set Therapeutic Obs' dialog accessible
+        from the patient form. This method exists purely to stop that button
+        from blowing up.
+
+        Pressing the button creates the therapeutic level
+        record even without calling anything (I think the `oe_form_button_save`
+        class on the button triggers an action in Odoo's JavaScript) but if
+        there is nothing set on the button to call then it blows up during
+        module load.
+
+        :param cr:
+        :param uid:
+        :param ids:
+        :param context:
+        :return:
+        """
         pass
+
+    @api.constrains('level', 'frequency')
+    def _validate(self):
+        self._validate_level_range()
+        self._validate_frequency_is_only_set_on_level_2()
+
+    def _validate_level_range(self):
+        validate.in_min_max_range(1, 3, self.level)
+
+    def _validate_frequency_is_only_set_on_level_2(self):
+        if self.level == 2 and not self.frequency:
+            raise ValidationError("Must have a frequency set for level 2 "
+                                  "therapeutic obs.")
