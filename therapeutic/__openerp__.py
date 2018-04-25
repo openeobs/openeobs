94b9336bf85f40a26c6048d37474d65964bf8852
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-04-25 11:13:37 +0100

    Added module description (to trigger build).

--- therapeutic/__openerp__.py
+++ therapeutic/__openerp__.py
@@ -4,7 +4,11 @@
     'category': 'Clinical',
     'license': 'AGPL-3',
     'summary': 'Adds therapeutic observations to LiveObs.',
-    'description': """""",
+    'description': """
+    Adds therapeutic observations to LiveObs. Therapeutic observations attempt 
+    to assess the general well-being of the patient by making note of things 
+    like their status and location.
+    """,
     'author': 'BJSS',
     'website': 'http://www.liveobs.com/',
     'depends': ['nh_eobs_mental_health'],
