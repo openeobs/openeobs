dd97c7c74bebbec65571ebb689a3313a32add168
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-05-10 14:45:45 +0100

    WIP adding therapeutic level info to patient form.

--- therapeutic/models/nh_clinical_wardboard.py
+++ therapeutic/models/nh_clinical_wardboard.py
@@ -5,33 +5,71 @@ class NhClinicalWardboard(models.Model):
 
     _inherit = 'nh.clinical.wardboard'
 
-    therapeutic_level = fields.Integer(
-        compute='_get_current_therapeutic_level')
+    therapeutic_level = fields.Char(
+        size=20,
+        compute='_set_therapeutic_fields'
+    )
     therapeutic_frequency = fields.Char(
-        compute='_get_current_therapeutic_frequency')
+        size=50,
+        compute='_set_therapeutic_fields'
+    )
+    therapeutic_staff_to_patient_ratio = fields.Char(
+        size=20,
+        compute='_set_therapeutic_fields'
+    )
 
     @api.depends('patient_id')
-    def _get_current_therapeutic_level(self):
+    def _set_therapeutic_fields(self):
         for record in self:
             current_therapeutic_level = \
                 record._get_latest_therapeutic_level_record()
-            record.therapeutic_level = current_therapeutic_level.level
+            if not current_therapeutic_level:
+                record.therapeutic_level = 'Not set'
+                return
 
-    @api.depends('patient_id')
-    def _get_current_therapeutic_frequency(self):
-        for record in self:
-            current_therapeutic_level = \
-                record._get_latest_therapeutic_level_record()
-            frequency_display = '{} min(s)'.format(
-                current_therapeutic_level.frequency)
-            record.therapeutic_frequency = frequency_display
+            self._set_therapeutic_level(record, current_therapeutic_level)
+            self._set_therapeutic_frequency(record, current_therapeutic_level)
+            self._set_therapeutic_staff_to_patient_ratio(
+                record, current_therapeutic_level
+            )
 
     def _get_latest_therapeutic_level_record(self):
-        domain = [
-            ('patient', '=', self.patient_id.id)
-        ]
+        domain = [('patient', '=', self.patient_id.id)]
         therapeutic_level_model = \
-            self.env['nh.clinical.patient.observation.therapeutic.level']
+            self.env['nh.clinical.therapeutic.level']
         current_therapeutic_level = therapeutic_level_model.search(
             domain, order='id desc', limit=1)
         return current_therapeutic_level
+
+    @staticmethod
+    def _set_therapeutic_level(record, current_therapeutic_level):
+        therapeutic_level = current_therapeutic_level.level
+        record.therapeutic_level = therapeutic_level
+
+    def _set_therapeutic_frequency(self, record, current_therapeutic_level):
+        therapeutic_level_model = self.env['nh.clinical.therapeutic.level']
+        frequency_int = current_therapeutic_level.frequency
+        frequencies = therapeutic_level_model.frequencies
+
+        # Lookup display string using integer.
+        frequency_selection = filter(
+            lambda frequency: frequency[0] == frequency_int, frequencies
+        )[0]
+        frequency_str = frequency_selection[1]
+        record.therapeutic_frequency = frequency_str
+
+    def _set_therapeutic_staff_to_patient_ratio(
+            self, record, current_therapeutic_level):
+        therapeutic_level_model = self.env['nh.clinical.therapeutic.level']
+        staff_to_patient_ratio_int = \
+            current_therapeutic_level.staff_to_patient_ratio
+        staff_to_patient_ratios = \
+            therapeutic_level_model.staff_to_patient_ratios
+
+        # Lookup display string using integer.
+        staff_to_patient_ratio_selection = filter(
+            lambda frequency: frequency[0] == staff_to_patient_ratio_int,
+            staff_to_patient_ratios
+        )[0]
+        staff_to_patient_ratio_str = staff_to_patient_ratio_selection[1]
+        record.therapeutic_staff_to_patient_ratio = staff_to_patient_ratio_str
